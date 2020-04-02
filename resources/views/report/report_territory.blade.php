3a95e9b3a774bb2fb7f83b646c41a452eb8725dd
rama11 <prof.agastyo@gmail.com>
2020-04-02 11:50:59 +0700

    change filter format

--- resources/views/report/report_territory.blade.php
+++ resources/views/report/report_territory.blade.php
@@ -287,6 +287,11 @@
       // })
 
       $('.dates').daterangepicker({
+        startDate: moment().startOf('year'),
+        endDate  : moment().endOf('year'),
+        locale: {
+          format: 'DD/MM/YYYY'
+        }
       },function (start, end) {
           start: moment();
           end  : moment();
@@ -313,19 +318,18 @@
       $('#daterange-btn').daterangepicker(
         {
           ranges   : {
-            'Today'       : [moment(), moment()],
-            'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-            'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-            'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            'This Month'   : [moment().startOf('month'), moment().endOf('month')],
+            'Last 3 Month' : [moment().startOf('month').subtract(3, 'months'), moment().endOf('month')],
+            'Last 6 Month' : [moment().startOf('month').subtract(6, 'months'), moment().endOf('month')],
+            'Last Year'    : [moment().startOf('year').subtract(1, 'year'),moment().endOf('year').subtract(1, 'year')],
+            'This Year'    : [moment().startOf('year'),moment().endOf('year')],
           },
-          startDate: moment().subtract(29, 'days'),
-          endDate  : moment()
+          locale: {
+            format: 'DD/MM/YYYY'
+          }
         },
         function (start, end) {
-          console.log(start)
-          $('#reportrange').val(start.format('MM/DD/YYYY') + ' - ' + end.format('MM/DD/YYYY'))
+          $('#reportrange').val(start.format('DD/MM/YYYY') + ' - ' + end.format('DD/MM/YYYY'))
 
           start_date  = start.format("YYYY-MM-DD HH:mm:ss");
           end_date    = end.format("YYYY-MM-DD HH:mm:ss");
