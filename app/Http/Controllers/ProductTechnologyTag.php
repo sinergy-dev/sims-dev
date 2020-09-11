6676b04a80aea07a5c49d986243b5602db5de478
rama11 <prof.agastyo@gmail.com>
2020-09-11 16:20:00 +0700

    add product technology controller

---
+++ app/Http/Controllers/ProductTechnologyTag.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use DB;
+use Illuminate\Http\Request;
+use App\ProductTag;
+use App\ProductTagRelation;
+use App\TechnologyTag;
+use App\TechnologyTagRelation;
+use Carbon\Carbon;
+
+class ProductTechnologyTag extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(){
+    	$nik = Auth::User()->nik;
+        $territory = DB::table('users')->select('id_territory')->where('nik', $nik)->first();
+        $ter = $territory->id_territory;
+        $division = DB::table('users')->select('id_division')->where('nik', $nik)->first();
+        $div = $division->id_division;
+        $position = DB::table('users')->select('id_position')->where('nik', $nik)->first();
+        $pos = $position->id_position;
+
+    	if ($ter != null) {
+            $notif = DB::table('sales_lead_register')
+            ->select('opp_name','nik')
+            ->where('result','OPEN')
+            ->orderBy('created_at','desc')
+            ->get();
+        }elseif ($div == 'TECHNICAL PRESALES' && $pos == 'STAFF') {
+            $notif = DB::table('sales_lead_register')
+            ->join('sales_solution_design', 'sales_solution_design.lead_id', '=', 'sales_lead_register.lead_id')
+            ->select('sales_lead_register.opp_name','sales_solution_design.nik')
+            ->where('result','')
+            ->orderBy('sales_lead_register.created_at','desc')
+            ->get();
+        }else{
+            $notif = DB::table('sales_lead_register')
+            ->select('opp_name','nik')
+            ->where('result','OPEN')
+            ->orderBy('created_at','desc')
+            ->get();
+        }
+
+        if ($div == 'TECHNICAL PRESALES' && $pos == 'MANAGER') {
+            $notifOpen= DB::table('sales_lead_register')
+            ->join('sales_solution_design', 'sales_solution_design.lead_id', '=', 'sales_lead_register.lead_id')
+            ->select('sales_lead_register.opp_name','sales_solution_design.nik','sales_solution_design.lead_id')
+            ->where('result','')
+            ->orderBy('sales_lead_register.created_at','desc')
+            ->get();
+        }elseif ($div == 'TECHNICAL PRESALES' && $pos == 'STAFF') {
+            $notifOpen= DB::table('sales_lead_register')
+            ->join('sales_solution_design', 'sales_solution_design.lead_id', '=', 'sales_lead_register.lead_id')
+            ->select('sales_lead_register.opp_name','sales_solution_design.nik','sales_solution_design.lead_id')
+            ->where('result','')
+            ->orderBy('sales_lead_register.created_at','desc')
+            ->get();
+        }elseif ($div == 'SALES' && $pos == 'MANAGER') {
+            $notifOpen= DB::table('sales_lead_register')
+            ->select('opp_name','nik','lead_id')
+            ->where('result','')
+            ->orderBy('created_at','desc')
+            ->get();
+        }elseif ($div == 'SALES' && $pos == 'STAFF') {
+            $notifOpen= DB::table('sales_lead_register')
+            ->select('opp_name','nik','lead_id')
+            ->where('result','')
+            ->orderBy('created_at','desc')
+            ->get();
+        }else{
+            $notifOpen= DB::table('sales_lead_register')
+            ->join('sales_solution_design', 'sales_solution_design.lead_id', '=', 'sales_lead_register.lead_id')
+            ->select('sales_lead_register.opp_name','sales_solution_design.nik','sales_solution_design.lead_id')
+            ->where('result','')
+            ->orderBy('sales_lead_register.created_at','desc')
+            ->get();
+        }
+
+        if ($div == 'TECHNICAL PRESALES' && $pos == 'MANAGER') {
+            $notifsd= DB::table('sales_lead_register')
+            ->join('sales_solution_design', 'sales_solution_design.lead_id', '=', 'sales_lead_register.lead_id')
+            ->select('sales_lead_register.opp_name','sales_solution_design.nik','sales_lead_register.lead_id')
+            ->where('result','SD')
+            ->orderBy('sales_lead_register.created_at','desc')
+            ->get();
+        }elseif ($div == 'TECHNICAL PRESALES' && $pos == 'STAFF') {
+            $notifsd= DB::table('sales_lead_register')
+            ->join('sales_solution_design', 'sales_solution_design.lead_id', '=', 'sales_lead_register.lead_id')
+            ->select('sales_lead_register.opp_name','sales_solution_design.nik','sales_lead_register.lead_id')
+            ->where('result','SD')
+            ->orderBy('sales_lead_register.created_at','desc')
+            ->get();
+        }elseif ($div == 'SALES' && $pos == 'MANAGER') {
+            $notifsd= DB::table('sales_lead_register')
+            ->select('opp_name','nik','lead_id')
+            ->where('result','SD')
+            ->orderBy('created_at','desc')
+            ->get();
+        }elseif ($div == 'SALES' && $pos == 'STAFF') {
+            $notifsd= DB::table('sales_lead_register')
+            ->select('opp_name','nik','lead_id')
+            ->where('result','SD')
+            ->orderBy('created_at','desc')
+            ->get();
+        }else{
+            $notifsd= DB::table('sales_lead_register')
+            ->join('sales_solution_design', 'sales_solution_design.lead_id', '=', 'sales_lead_register.lead_id')
+            ->select('sales_lead_register.opp_name','sales_solution_design.nik','sales_lead_register.lead_id')
+            ->where('result','SD')
+            ->orderBy('sales_lead_register.created_at','desc')
+            ->get();
+        }
+
+        if ($div == 'TECHNICAL PRESALES' && $pos == 'MANAGER') {
+            $notiftp= DB::table('sales_lead_register')
+            ->join('sales_solution_design', 'sales_solution_design.lead_id', '=', 'sales_lead_register.lead_id')
+            ->select('sales_lead_register.opp_name','sales_solution_design.nik','sales_solution_design.lead_id')
+            ->where('result','TP')
+            ->orderBy('sales_lead_register.created_at','desc')
+            ->get();
+        }elseif ($div == 'TECHNICAL PRESALES' && $pos == 'STAFF') {
+            $notiftp= DB::table('sales_lead_register')
+            ->join('sales_solution_design', 'sales_solution_design.lead_id', '=', 'sales_lead_register.lead_id')
+            ->select('sales_lead_register.opp_name','sales_solution_design.nik','sales_solution_design.lead_id')
+            ->where('result','TP')
+            ->orderBy('sales_lead_register.created_at','desc')
+            ->get();
+        }elseif ($div == 'SALES' && $pos == 'MANAGER') {
+            $notiftp= DB::table('sales_lead_register')
+            ->select('opp_name','nik','lead_id')
+            ->where('result','TP')
+            ->orderBy('created_at','desc')
+            ->get();
+        }elseif ($div == 'SALES' && $pos == 'STAFF') {
+            $notiftp= DB::table('sales_lead_register')
+            ->select('opp_name','nik','lead_id')
+            ->where('result','TP')
+            ->orderBy('created_at','desc')
+            ->get();
+        }else{
+            $notiftp= DB::table('sales_lead_register')
+            ->join('sales_solution_design', 'sales_solution_design.lead_id', '=', 'sales_lead_register.lead_id')
+            ->select('sales_lead_register.opp_name','sales_solution_design.nik','sales_solution_design.lead_id')
+            ->where('result','TP')
+            ->orderBy('sales_lead_register.created_at','desc')
+            ->get();
+        }
+
+        if (Auth::User()->id_position == 'ADMIN') {
+            $notifClaim = DB::table('dvg_esm')
+                            ->select('nik_admin', 'personnel', 'type')
+                            ->where('status', 'ADMIN')
+                            ->get();
+        } elseif (Auth::User()->id_position == 'HR MANAGER') {
+            $notifClaim = DB::table('dvg_esm')
+                            ->select('nik_admin', 'personnel', 'type')
+                            ->where('status', 'HRD')
+                            ->get();
+        } elseif (Auth::User()->id_division == 'FINANCE') {
+            $notifClaim = DB::table('dvg_esm')
+                            ->select('nik_admin', 'personnel', 'type')
+                            ->where('status', 'FINANCE')
+                            ->get();
+        }
+
+        $product = ProductTag::all();
+
+        $technology = TechnologyTag::all(); 
+
+    	return view('sales/tag_product_technology',compact('lead','notif','notifOpen','notifsd','notiftp', 'notifClaim','product','technology'));
+    
+    }
+
+    public function store_product(Request $req){
+        $store = new ProductTag();
+        $store->name_product = $req->name_product;
+        $store->description_product = $req->desc_product;
+        $store->date_add = Carbon::now()->toDateTimeString();
+        $store->save(); 
+
+        return redirect('sales/tag')->with('success', 'Product has been Added!');
+    }
+
+    public function store_tech(Request $req){
+        $store = new TechnologyTag();
+        $store->name_tech = $req->name_tech;
+        $store->desc_tech = $req->desc_tech;
+        $store->date_add = Carbon::now()->toDateTimeString();
+        $store->save(); 
+
+        return redirect('sales/tag')->with('success', 'Technology has been Added!');
+    }
+
+    public function update_tag_product(Request $req){
+        $update = ProductTag::where('id',$req->id_product_edit)->first();
+        $update->name_product = $req->name_product_edit;
+        $update->description_product = $req->desc_product_edit;
+        $update->update(); 
+
+        return redirect('sales/tag')->with('update', 'Product has been Updated!');
+        
+    }
+
+    public function update_tag_tech(Request $req){
+        $update = TechnologyTag::where('id',$req->id_tech_edit)->first();
+        $update->name_tech = $req->name_tech_edit;
+        $update->desc_tech = $req->desc_tech_edit;
+        $update->update(); 
+
+        return redirect('sales/tag')->with('update', 'Technology has been Updated!');
+    }
+
+    public function detail_product(Request $req){
+        return array(ProductTag::all()->where('id',$req->id_product_tag)->first());
+    }
+
+    public function detail_tech(Request $req){
+        return array(TechnologyTag::all()->where('id',$req->id_tech_tag)->first());
+    }
+}
