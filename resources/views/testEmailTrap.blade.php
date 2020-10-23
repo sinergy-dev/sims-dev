bfcf849f191830591bb2d323f9513bb081be76d4
rama11 <prof.agastyo@gmail.com>
2020-10-23 08:56:16 +0700

    add mail testing to mailtrap

---
+++ resources/views/testEmailTrap.blade.php
@@ -0,0 +1,23 @@
+<div style="color: #141414;font-family: 'Source Sans Pro','Helvetica Neue',Helvetica,Arial,sans-serif;">
+	<p>Dear All,</p>
+
+	<p>This is testing email from Mailable</p>
+
+	<p>
+		Mohon di periksa kembali, jika ada kesalahan atau pertanyaan silahkan hubungi Team Developer (Ext: 384) atau email ke development@sinergy.co.id.
+	</p>
+	<p>
+		Thanks<br>
+		Best Regard,
+	</p>
+	<h5 style="color: #f39c12 !important;margin-top: 0px" class="text-yellow" ><i>Tech - Dev</i></h5>
+	<p>
+		----------------------------------------<br>
+		PT. Sinergy Informasi Pratama (SIP)<br>
+		| Inlingua Building 2nd Floor |<br>
+		| Jl. Puri Raya, Blok A 2/3 No. 33-35 | Puri Indah |<br>
+		| Kembangan | Jakarta 11610 – Indonesia |<br>
+		| Phone | 021 - 58355599 |<br>
+		----------------------------------------<br>
+	</p>
+</div>
