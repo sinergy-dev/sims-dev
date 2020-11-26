a5b661402d85f29e5931f379386dcc423d9ce968
rama11 <prof.agastyo@gmail.com>
2020-11-26 11:00:50 +0700

    update status karyawan

--- app/Console/Commands/UpdateStatusKaryawan.php
+++ app/Console/Commands/UpdateStatusKaryawan.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\User;
+use GuzzleHttp\Client;
 use DB;
 
 class UpdateStatusKaryawan extends Command
@@ -39,21 +40,44 @@ class UpdateStatusKaryawan extends Command
      */
     public function handle()
     {
-
+        // $client = new Client();
+        // $client = $client->get('https://www.googleapis.com/calendar/v3/calendars/en.indonesian%23holiday%40group.v.calendar.google.com/events?key=' . env('GOOGLE_API_YEY'));
+        // $variable = json_decode($client->getBody())->items;
+        // $i = 0;
+        // foreach ($variable as $key => $value) {
+        //   if(strpos($value->summary,'Cuti Bersama') === 0){
+        //     if(strpos($value->start->date , date('Y')) === 0){
+        //       $i++;
+        //     }
+        //   }
+        // }
+        // $total_cuti = 12 - $i;
+        // $reset = User::select('nik','name','status_karyawan')->where('nik',1191094060)->first();
         $reset = User::select('nik','name',DB::raw('DATEDIFF(NOW(),date_of_entry) AS date_of_entrys'),'status_karyawan')->get();
 
         foreach ($reset as $data) {
             // print_r($data->name . $data->nik . "\n");
-            if ($data->date_of_entrys >= '365') {
-                if ($data->status_karyawan == 'belum_cuti') {
-                    $update = User::where('nik',$data->nik)->first();
-                    $data->status_karyawan = 'cuti';
-                    $data->update();
-                    syslog(LOG_ERR, $update->nik . " - " . $update->name . "leaving permit permission updated ");
-                }
+            // if ($data->date_of_entrys >= '365') {
+            //     if ($data->status_karyawan == 'belum_cuti') {
+            //         $update = User::where('nik',$data->nik)->first();
+            //         $update->status_karyawan = 'cuti';
+            //         $update->update();
+            //         syslog(LOG_ERR, $update->nik . " - " . $update->name . "leaving permit permission updated ");
+            //     }
+            // }
+            if ($data->status_karyawan == 'belum_cuti') {
+                $update = User::where('nik',$data->nik)->first();
+                $update->status_karyawan = 'cuti';
+                $update->update();
+                syslog(LOG_ERR, $update->nik . " - " . $update->name . "leaving permit permission updated ");
             }
             
         }
+        // $update = User::where('nik',$reset->nik)->first();
+        // $update->status_karyawan = 'cuti';
+        // $update->cuti2 = $total_cuti;
+        // $update->update();
+
 
         syslog(LOG_ERR, "All user has been check for leaving permit");
 
