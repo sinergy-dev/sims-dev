ce450991ab3cd60d6dd906286383cc3d70fa4e31
rama11 <prof.agastyo@gmail.com>
2020-12-28 20:50:07 +0700

    add roole trait to user model

--- app/User.php
+++ app/User.php
@@ -2,11 +2,14 @@
 
 namespace App;
 
+use HttpOz\Roles\Traits\HasRole;
+use HttpOz\Roles\Contracts\HasRole as HasRoleContract;
+
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-class User extends Authenticatable
+class User extends Authenticatable implements HasRoleContract
 {
-    use Notifiable;
+    use Notifiable, HasRole;
 
     /**
      * The attributes that are mass assignable.
