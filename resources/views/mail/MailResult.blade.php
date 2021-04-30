d70ac2b75c0a27b8b06bf45614c00f5011f07af8
rama11 <prof.agastyo@gmail.com>
2021-04-30 09:23:28 +0000

    enhance view mailresult.blade

--- resources/views/mail/MailResult.blade.php
+++ resources/views/mail/MailResult.blade.php
@@ -1,3 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+	<style type="text/css">
+		table {
+		  border-collapse: collapse;
+		}
+
+		table, th, td {
+		  border: 0px solid grey;
+		  padding-top: 14px;
+		}
+
+		table, th {
+			padding-left: 15px;
+		}
+
+		#bg_ket {
+			border-radius: 10px;
+		}
+
+		#txt_center {
+			text-align: center;
+		}
+
+		.money:before{
+			content:"Rp";
+		}
+
+		/*.centered{
+			position: absolute;
+		  	top: 50%;
+		  	left: 50%;
+		  	transform: translate(-50%, -40%);
+		}*/
+	</style>
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="{{asset('js/jquery.mask.min.js')}}"></script>
+	<script type="text/javascript" src="{{asset('js/jquery.mask.js')}}"></script>
+	<script type="text/javascript">
+		console.log( $("#amounts").text())
+	    $('.money').mask('000,000,000,000,000', {reverse: true});
+	</script>
+</head>
+<body style="display: block; width: 600px; margin-left: auto; margin-right: auto; color: #000000">
+	<div style="line-height: 1.5em">
+		<img src="{{ asset('image/sims_sangar_2.png')}}" style="width: 30%; height: 30%">
+	</div>
+	<div style="line-height: 1.5em;padding-left: 13px;">
+		<div style="font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+			<p style="font-size: 20px">
+				Dear Bu {{$users->name}},
+			</p>
+			<p style="font-size: 16px">
+				Please create a Project ID for the project with the following details:
+			</p>
+			<div id="bg_ket" style="background-color: #ececec; padding: 10px">
+				<table style="text-align: left;margin: 5px; font-size: 16px" class="tableLead">
+					<tr>
+						<th>Lead Id</th>
+						<th> : </th>
+						<td>{{$pid_info->lead_id}}</td>
+					</tr>
+					<tr>
+						<th>Nama Project</th>
+						<th> : </th>
+						<td>{{$pid_info->opp_name}}</td>
+					</tr>
+					<tr>
+						<th>Nama Sales</th>
+						<th> : </th>
+						<td>{{$pid_info->name}}</td>
+					</tr>
+					<tr>
+						<th>Amount</th>
+						<th> : </th>
+						<td>{{$pid_info->amount_pid}}</td>
+					</tr>
+					<tr>
+						<th>No. PO</th>
+						<th> : </th>
+						<td>{{$pid_info->no_po}}</td>
+					</tr>
+					<tr>
+						<th>No Quote</th>
+						<th> : </th>
+						<td>{{$pid_info->quote_number2}}</td>
+					</tr>
+				</table>
+			</div>
+			<p style="font-size: 16px">
+				Click the following link button to create a Project ID.
+			</p>
+
+			<center>
+				<a href="{{url($pid_info->url_create)}}" class="btn btn-info" role="button">Create ID Project</a>
+			</center>
+
+			<p style="font-size: 16px">
+				Please check again, if there are errors or questions please contact the Developer Team (Ext: 384) or email to development@sinergy.co.id.<br>
+				Thank you.
+			</p>
+			<p style="font-size: 16px">
+				Best Regard,
+			</p><br>
+			<p style="font-size: 16px">
+				Tech - Dev
+			</p>
+		</div>
+	</div>
+</body>
+<footer style="display:block;width:600px;margin-left:auto;margin-right:auto;">
+	<div style="background-color: #7868e6; padding: 20px; color: #ffffff; font-size: 12px; font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+		<p>
+			<center>PT. Sinergy Informasi Pratama</center>
+		</p>
+		<p>
+			<center>Jl. Puri Raya, Blok A 2/3 No. 33-35 Puri Indah, Kembangan, Jakarta, Indonesia 11610</center>
+		</p>
+		<p>
+			<center><i class="fa fa-phone"></i>021 - 58355599</center>
+		</p>
+	</div>
+</footer>
+</html>
+
+
+<!-- 
 <div style="color: #141414;font-family: 'Source Sans Pro','Helvetica Neue',Helvetica,Arial,sans-serif;">
 	<p>
 		Dear Bu {{$users->name}},
@@ -47,6 +176,7 @@
 								Create ID Project
 							</a>
 						</td>
+
 					</tr>
 				</table>
 			</td>
@@ -69,4 +199,4 @@
 		| Phone | 021 - 58355599 |<br>
 		----------------------------------------<br>
 	</p>
-</div>
+</div> -->
