767d96975b459b4508f5f92e300859491399ffb9
rama11 <prof.agastyo@gmail.com>
2021-04-30 07:58:00 +0000

    add initmenubase

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -202,7 +202,7 @@ class QuoteController extends Controller
 
         $year_before = Quote::select(DB::raw('YEAR(created_at) year'))->orderBy('year','desc')->groupBy('year')->get();
 
-        return view('quote/quote',compact('notif','datas','notifOpen','notifsd','notiftp', 'notifClaim', 'counts', 'count','pops', 'pops2', 'backdate_num', 'sidebar_collapse', 'customer', 'status_quote','tahun','year_before'));
+        return view('quote/quote',compact('notif','datas','notifOpen','notifsd','notiftp', 'notifClaim', 'counts', 'count','pops', 'pops2', 'backdate_num', 'sidebar_collapse', 'customer', 'status_quote','tahun','year_before'))->with(['initView'=> $this->initMenuBase()]);
 	}
 
 	public function create()
