d561d869edc32076f8526217485370b1535a66d9
rama11 <prof.agastyo@gmail.com>
2021-05-19 03:40:31 +0000

    inisialisasi

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\User;
+use App\Sales;
 use Mail;
 use App\Mail\EmailRemainderWeekly;
 use App\Mail\MailResult;
 use App\Mail\mailPID;
+use App\Mail\CreateLeadRegister;
+use App\Mail\AssignPresales;
+use App\Mail\RaiseTender;
+
 use App\Mail\RequestNewAssetHr;
 use App\Notifications\Testing;
 use Notification;
@@ -22,6 +27,8 @@ use Google_Client;
 use Google_Service_Drive;
 use Google_Service_Calendar;
 use Google_Service_Calendar_Event;
+
+use HttpOz\Roles\Models\Role;
 
 // use App\Notifications\Result;
 
@@ -37,13 +44,77 @@ class TestController extends Controller
   			$users = User::select('name')->where('id_division','FINANCE')->where('id_position','MANAGER')->first();
 
   			$pid_info = Sales::join('sales_tender_process','sales_tender_process.lead_id','=','sales_lead_register.lead_id')
-                  ->join('tb_pid','tb_pid.lead_id','=','sales_lead_register.lead_id')->select('lead_id','no_po','amount_pid','quote_number2')->first(); 
+                  ->join('tb_pid','tb_pid.lead_id','=','sales_lead_register.lead_id')
+                  ->select('sales_lead_register.lead_id','no_po','amount_pid','quote_number2')
+                  ->first(); 
             // Mail::to($users, new Result());
 
-        foreach ($to as $data) {
-        	Mail::to($data->email)->send(new MailResult($users,$pid_info));
-        }
+        // foreach ($to as $data) {
+        // 	Mail::to($data->email)->send(new MailResult($users,$pid_info));
+        // }
+                  $pid_info->url_create = "hahahaha";
+        return new MailResult($users,$pid_info);
+
+        // return Mail::to('tito@sinergy.co.id')->send(new MailResult($users,$pid_info));
 	}
+
+  public function testNewLead(){
+    // $data = DB::table('sales_lead_register')
+    //                 ->join('users', 'users.nik', '=', 'sales_lead_register.nik')
+    //                 ->join('tb_contact', 'sales_lead_register.id_customer', '=', 'tb_contact.id_customer')
+    //                 ->select('sales_lead_register.lead_id','tb_contact.customer_legal_name', 'sales_lead_register.opp_name','sales_lead_register.amount', 'users.name')
+    //                 ->where('lead_id','AEON200201')
+    //                 ->first();
+    $data = DB::table('sales_lead_register')
+      ->join('users', 'users.nik', '=', 'sales_lead_register.nik')
+      ->join('tb_contact', 'sales_lead_register.id_customer', '=', 'tb_contact.id_customer')
+      ->select(
+          'sales_lead_register.lead_id',
+          'tb_contact.customer_legal_name', 
+          'sales_lead_register.opp_name',
+          'sales_lead_register.amount', 
+          'users.name')
+      ->where('lead_id',"BMRI210403")
+      ->first();
+    return new CreateLeadRegister($data); 
+
+    // return Mail::to('tito@sinergy.co.id')->send(new CreateLeadRegister($data));
+
+  }
+
+  public function testAssignPresales(){
+    $data = DB::table('sales_lead_register')
+                    ->join('sales_solution_design','sales_solution_design.lead_id','sales_lead_register.lead_id')
+                    ->join('users as sales', 'sales.nik', '=', 'sales_lead_register.nik')
+                    ->join('users as presales','presales.nik','=','sales_solution_design.nik')
+                    ->join('tb_contact', 'sales_lead_register.id_customer', '=', 'tb_contact.id_customer')
+                    ->select('sales_lead_register.lead_id','tb_contact.customer_legal_name', 'sales_lead_register.opp_name','sales_lead_register.amount', 'sales.name as sales_name','presales.name as presales_name')
+                    ->where('sales_lead_register.lead_id','AEON200201')
+                    ->first();
+    $status = 'reAssign';
+
+
+    return new AssignPresales($data,$status);
+
+    // return Mail::to('hisyam@sinergy.co.id')->send(new AssignPresales($data,$status));
+
+  }
+
+  public function testRaiseToTender(){
+    $data = DB::table('sales_lead_register')
+                    ->join('sales_solution_design','sales_solution_design.lead_id','sales_lead_register.lead_id')
+                    ->join('users as sales', 'sales.nik', '=', 'sales_lead_register.nik')
+                    ->join('users as presales','presales.nik','=','sales_solution_design.nik')
+                    ->join('tb_contact', 'sales_lead_register.id_customer', '=', 'tb_contact.id_customer')
+                    ->select('sales_lead_register.lead_id','tb_contact.customer_legal_name', 'sales_lead_register.opp_name','sales_lead_register.amount', 'sales.name as sales_name','presales.name as presales_name')
+                    ->where('sales_lead_register.lead_id','AEON200201')
+                    ->first();
+
+    // return Mail::to('tito@sinergy.co.id')->send(new RaiseTender($data));
+
+    return new RaiseTender($data);      
+
+  }
 
   public function testEmailPeminjaman(){
 
@@ -79,6 +150,8 @@ class TestController extends Controller
 
     $return =  new EmailRemainderWeekly($parameterEmail);
     Mail::to('agastya@sinergy.co.id')->send($return);
+
+    // return Mail::to('tito@sinergy.co.id')->send(new EmailRemainderWeekly($parameterEmail));
 
     return $return;
   }
@@ -433,6 +506,267 @@ class TestController extends Controller
       file_put_contents($tokenPath, json_encode($client->getAccessToken()));
       $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . '/';
       return redirect()->away($redirect_uri);
+    }
+  }
+
+  public function testPermission(){
+    return view('testPermission')->with(['initView'=> $this->initMenuBase()]);
+  }
+
+  public function testPermissionConfig(){ 
+    return view('testPermissionConfig')->with(['initView'=> $this->initMenuBase()]);
+  }
+
+  public function getUserList(){
+    return DB::table('role_user')
+      ->select(
+          'role_user.*',
+          DB::raw('`roles`.`group` AS `name_group`'),
+          DB::raw('`roles`.`name` AS `name_roles`'),
+          'users.name',
+          'roles_feature_each.each_feature'
+        )
+      ->join('users','role_user.user_id','=','users.nik')
+      ->join('roles','role_user.role_id','=','roles.id')
+      ->joinSub(DB::table('roles_feature')
+        ->select(
+          'roles_feature.role_id',
+          DB::raw('GROUP_CONCAT(`features`.`name`) AS `each_feature`')
+        )
+        ->join('features','features.id','=','roles_feature.feature_id')
+        ->groupBy('roles_feature.role_id'), 'roles_feature_each', function ($join) {
+        $join->on('roles.id', '=', 'roles_feature_each.role_id');
+      })
+      // ->take(1)
+      ->get();
+  }
+
+  public function getParameter(){
+    $name = DB::table('users')
+      ->select(
+          DB::raw('`nik` AS `id`'),
+          DB::raw('`name` AS `text`'),
+        )
+      ->where('id_company','=','1')
+      ->where('status_karyawan','=','cuti')
+      ->get();
+
+    $roles = DB::table('roles')
+      ->select(
+        DB::raw('`id` AS `id`'),
+        DB::raw('`name` AS `text`'),
+        DB::raw('`group` AS `group`'),
+      )->get();
+
+      $groups = [];
+      foreach($roles->pluck('group')->unique()->values() as $group){
+        array_push($groups, ["text" => strtoupper($group),"children" => $roles->where('group',$group)->values()]);
+      }
+
+      // return collect(["result" => ["text" => "Sales","children" => [["id"=>1],["id"=>2]]]]);
+      // return $groups;
+    return collect(['name' => $name,'roles' => $groups]);
+  }
+
+  public function getParameterRoles(){
+      $roles = DB::table('roles')
+      ->select(
+        DB::raw('`id` AS `id`'),
+        DB::raw('`name` AS `text`'),
+        DB::raw('`group` AS `group`'),
+      )->get();
+
+      $groups = [];
+      foreach($roles->pluck('group')->unique()->values() as $group){
+        array_push($groups, ["text" => strtoupper($group),"children" => $roles->where('group',$group)->values()]);
+      }
+      // return collect(["result" => ["text" => "Sales","children" => [["id"=>1],["id"=>2]]]]);
+      // return $groups;
+    return collect(['roles' => $groups]);
+  }
+
+  public function getParameterFeature(Request $req){
+      $features = DB::table('features')
+      ->select(
+        DB::raw('`features`.`id` AS `id`'),
+        DB::raw('`name` AS `text`'),
+        DB::raw('`group` AS `group`'),
+      )->whereNotIn('id', function($query) use ($req){
+        $query->select('feature_id')
+              ->from('roles_feature')->where('role_id',$req->roles_id);
+      })
+      ->get();
+
+      $groupsFeature = [];
+      foreach($features->pluck('group')->unique()->values() as $group){
+        array_push($groupsFeature, ["text" => strtoupper($group),"children" => $features->where('group',$group)->values()]);
+      }
+
+      // return collect(["result" => ["text" => "Sales","children" => [["id"=>1],["id"=>2]]]]);
+      // return $groups;
+    return collect(['features' => $groupsFeature]);
+  }
+
+
+  public function getFeatureRole(){
+    return DB::table('roles')
+      ->select(
+          DB::raw('`roles`.`group` AS `name_group`'),
+          DB::raw('`roles`.`name` AS `name_roles`'),
+          DB::raw('`roles_feature_each`.`each_feature` AS `feature_name`')
+        )
+      // ->join('features','features.id','=','roles_feature.feature_id')
+      ->joinSub(DB::table('roles_feature')
+        ->select(
+          'roles_feature.role_id',
+          DB::raw('GROUP_CONCAT(`features`.`name`) AS `each_feature`')
+        )
+        ->join('features','features.id','=','roles_feature.feature_id')
+        ->groupBy('roles_feature.role_id'), 'roles_feature_each', function ($join) {
+        $join->on('roles.id', '=', 'roles_feature_each.role_id');
+      })
+      ->get();
+    // return DB::table('roles')->get();
+  }
+
+  public function setRoles(Request $req){
+    foreach ($req->id_role as $id_role) {
+      User::find($req->id_user)->attachRole(Role::find($id_role));
+    }
+    // return $req->id_role;
+    return "Success";
+  }
+
+  public function setRolesFeature(Request $req){
+    foreach ($req->id_feature as $id_feature) {
+      DB::table('roles_feature')->insert([
+        'role_id' => Role::find($req->id_role)->id,
+        'feature_id' => $id_feature
+      ]);
+      // Role::find($req->id_role)->attachRole(DB::table('features')->find($id_feature));
+    }
+    // return $req->id_role;
+    return "Success";
+  }
+
+  public function addConfigRoles(Request $req){
+    DB::table('roles')->insert([
+        'name' => $req->name,
+        'slug' => $req->slug,
+        'description' => $req->description,
+        'group' => $req->group
+    ]);
+
+    return "Success";
+  }
+
+  public function addConfigFeature(Request $req){
+    $index_of = DB::table('features')->latest('index_of')->first()->index_of + 1;
+
+    DB::table('features')->insert([
+        'name' => $req->name,
+        'description' => $req->description,
+        'group' => $req->group,
+        'index_of'=> $index_of,
+        'url'=>$req->url
+    ]);
+
+    return "Success";
+  }
+
+  public function getRoles(Request $req){
+    return DB::table('roles')->get();
+  }
+
+  public function getFeature(Request $req){
+    return DB::table('features')->get();
+  }
+
+  public function getRoleDetail(Request $req){
+    return collect([
+      'role' => DB::table('roles')->where('id','=',$req->id)->get()->first(),
+      'holder' => DB::table('role_user')
+        ->select('users.name')
+        ->join('users','role_user.user_id','=','users.nik')
+        ->where('role_id','=',$req->id)
+        ->get()
+    ]);
+  }
+
+  public function getFeatureItem(Request $req){
+    $column = DB::table('roles')->selectRaw('`id`,`name` AS `title`, REPLACE(`slug`,".","_") AS `name`,CONCAT("condition_",REPLACE(`slug`,".","_")) AS `data`');
+    if($req->group == "all") {
+      $column2 = $column->get();
+    } else {
+      $column2 = $column->where('group','=',$req->group)
+        ->get();
+    }
+    
+
+    $column = $column2->map(function ($item, $key) {
+      $item->class = "text-center";
+      return $item;
+    });
+
+    // return $column;
+
+    $return = DB::table('feature_item')
+      ->select("feature_item.*");
+      
+    foreach ($column as $key => $value) {
+      $return = $return->addSelect($value->name . ".condition_" . $value->name);
+      $leftJoin = DB::table('roles_feature_item')
+        ->where('roles_id','=',$value->id);
+
+      $join = DB::table('feature_item')
+        ->selectRaw('`feature_item`.`id`, CONCAT("' . "<label class='switch'><input class='featureItemCheck' type='checkbox' id='" . $value->id . "-" . '",' . '`feature_item`.`id`' . ',"\' ",' . "IF(`roles_feature_item_filtered`.`id` > 0,'checked','')" . ',"' . "><span class='slider round'></span></label>" . '") AS `condition_' . $value->name . '`')
+        ->leftJoinSub($leftJoin,'roles_feature_item_filtered',function($join){
+          $join->on('roles_feature_item_filtered.feature_item_id','=','feature_item.id');
+        });
+      $return = $return->joinSub($join,$value->name,function($join) use ($value){
+        $join->on($value->name . '.id','=','feature_item.id');
+      });
+
+      // $return = $return->selectRaw('CONCAT("' . "<label class='switch'><input type='checkbox' id='checkbox1'><span class='slider round'></span></label>" . '") AS `' . $value->name . '`');
+      
+      // $return = $return->selectRaw('CONCAT("' . "<label class='switch'><input type='checkbox' id='checkbox1'><span class='slider round'></span></label>" . '") AS `' . $value->name . '`');
+    }
+
+    $column->prepend(['title' => "Item",'data' => "item_id"]);
+    $column->prepend(['title' => "Feature",'data' => "group"]);
+
+    return collect(['data' => $return->get(),'column' => $column]);
+
+    // return DB::table('feature_item')
+    //   ->selectRaw("*")
+    //   ->selectRaw('CONCAT("' . "<label class='switch'><input type='checkbox' id='checkbox1'><span class='slider round'></span></label>" . '") AS `director`')
+    //   ->selectRaw('CONCAT("' . "<label class='switch'><input type='checkbox' id='checkbox1'><span class='slider round'></span></label>" . '") AS `staff`')
+    //   ->selectRaw('CONCAT("' . "<label class='switch'><input type='checkbox' id='checkbox1'><span class='slider round'></span></label>" . '") AS `admin`')
+    //   ->selectRaw('CONCAT("' . "<label class='switch'><input type='checkbox' id='checkbox1'><span class='slider round'></span></label>" . '") AS `hrstaff`')
+    //   ->selectRaw('CONCAT("' . "<label class='switch'><input type='checkbox' id='checkbox1'><span class='slider round'></span></label>" . '") AS `hrga`')
+    //   ->selectRaw('CONCAT("' . "<label class='switch'><input type='checkbox' id='checkbox1'><span class='slider round'></span></label>" . '") AS `pmostaff`')
+    //   ->get();
+  }
+
+  public function getFeatureItemParameter(){
+    return DB::table('roles')->select('group')->groupBy('group')->pluck('group');
+  }
+
+  public function changeFeatureItem(Request $req){
+    $roleFeatureItem = DB::table('roles_feature_item')
+      ->where('roles_id','=',$req->role)
+      ->where('feature_item_id','=',$req->feature);
+
+    if($roleFeatureItem->exists()){
+      $roleFeatureItem->delete();
+      return "deleted";
+    } else {
+      DB::table('roles_feature_item')
+        ->insert([
+          'roles_id' => $req->role,
+          'feature_item_id' => $req->feature
+        ]);
+      return "added";
     }
   }
 
