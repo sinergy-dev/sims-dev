50b5a8a731ac7859193cecf5fcc2e9ea4c33b4a4
rama11 <prof.agastyo@gmail.com>
2021-05-19 04:04:22 +0000

    mailEscalateTicket blade

---
+++ app/Http/Controllers/mailEscalateTicket.blade.php
@@ -0,0 +1,125 @@
+<div style="color: #555;font-family: 'Source Sans Pro','Helvetica Neue',Helvetica,Arial,sans-serif;">
+	<p id="emailEscalateHeader" style="margin: 0 0 10px;box-sizing: border-box;font-size: 14px;line-height: 1.42857143;color: #555;font-family: 'Source Sans Pro','Helvetica Neue',Helvetica,Arial,sans-serif;font-weight: 400;">
+		Dear Brillyan Aditya,<br>
+		<br>
+
+		Dikarenakan ada sebuah kenadala yang kami alami dalam menyelesaikan permasalahan <b>Koneksi dari pc ke switch undentified - BPJS Kesehatan KC Jakarta Timur</b>, kami disini bermaksud untuk meminta bantuan dalam penyelesaian masalah tersebut. Sampai sejauh ini, analisa yang kami lakukan terdapat pada kolom Root Cause Analysis (RCA) di Trouble Tiket di bawah ini dan diharapkan dapat berguna untuk mencoba menyelesaikan masalah tersbut.
+	</p>
+	<table style="width: 400px;">
+		<tr>
+			<td style="width: 50px;">
+				<h3 style="font-size: 24px;"><b><i>Trouble Ticket</i></b></h3>
+			</td>
+			<td style="padding-left: 72px;">
+				<img src="{{asset('/img/22.jpg')}}">
+			</td>
+		</tr>
+	</table>
+	<br>
+	<table class="table table2" style="width: 400px;max-width: 100%;margin-bottom: 20px;border-spacing: 0;border-collapse: collapse;line-height: 1.42857143;vertical-align: top;">
+		<tr >
+			<th style=" border:1px solid #fff; padding: 3px; width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Ticket ID</th>
+			<td style=" border:1px solid; padding: 3px;" class="holderEscalateID"></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff; padding: 3px; width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Refrence</th>
+			<td style=" border:1px solid; padding: 3px;" class="holderEscalateRefrence"></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Customer</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateCustomer"></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">PIC</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalatePIC"></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Contact</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateContact"></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Problem</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateProblem"></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Location</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateLocation"></td>
+		</tr>
+		<tr style="display: none" class="holderEscalateIDATM2">
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">ID ATM</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateIDATM"></td>
+		</tr>
+		<!-- <tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Engineer</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateEngineer"></td>
+		</tr> -->
+		<tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Date</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateDate"></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Serial Number</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateSerial"></td>
+		</tr>
+		<tr style="display: none" class="holderEscalateIDATM3">
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Machine Type</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateType"></td>
+		</tr>
+		<tr style="display: none" class="holderEscalateIPMechine3">
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">IP Machine</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateIPMechine4"></td>
+		</tr>
+		<tr style="display: none" class="holderEscalateIPServer3">
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">IP Server</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateIPServer4"></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Severity</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateSeverity"></td>
+		</tr>
+		<!-- <tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Counter Measure</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateCounter"></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Rootcause</th>
+			<td  style=" border:1px solid; padding: 3px;" class="holderEscalateRoot"></td>
+		</tr> -->
+		<tr>
+			<th style=" border:1px solid #fff;  padding: 3px;width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Status</th>
+			<td  style=" border:1px solid; padding: 3px; text-align: center; background-color: #d33724;border-bottom: none;color:#FFFFFF;" class="holderEscalateStatus text-center bg-red-active"></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff; padding: 3px; width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Waktu</th>
+			<td style=" border:1px solid; padding: 3px; text-align: center; background-color: #d33724; border-top: none; color:#FFFFFF;" class="holderEscalateWaktu text-center bg-red-active" ></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff; padding: 3px; width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Note</th>
+			<td style=" border:1px solid; padding: 3px;" class="holderEscalateNote"></td>
+		</tr>
+		<tr>
+			<th style=" border:1px solid #fff; padding: 3px; width:120px;color: #fff;background-color: #337ab7;text-align: left;" class="bg-primary">Root Cause Analysis</th>
+			<td style=" border:1px solid; padding: 3px;" class="holderEscalateNote"></td>
+		</tr>
+	</table>
+	<br>
+	<p>
+		Dimohon untuk memberikan dukungan sepenuhnya dalam menyelesaikan masalah teserbut.
+		Demikian yang dapat saya sampaikan.
+
+		Thanks<br>
+		Best Regard,
+	</p>
+	<h4 style="color: #3c8dbc !important;margin-bottom: 0px" class="text-light-blue" >{{Auth::user()->name}}</h4>
+	<h5 style="color: #f39c12 !important;margin-top: 0px" class="text-yellow" ><i>Helpdesk</i></h5>
+	<p>
+		----------------------------------------<br>
+		PT. Sinergy Informasi Pratama (SIP)<br>
+		| Inlingua Building 2nd Floor |<br>
+		| Jl. Puri Raya, Blok A 2/3 No. 33-35 | Puri Indah |<br>
+		| Kembangan | Jakarta 11610 – Indonesia |<br>
+		| Mobile | {{Auth::user()->phone}} |<br>
+		| Phone | 021 - 58355599 |<br>
+		----------------------------------------<br>
+	</p>
+</div>
