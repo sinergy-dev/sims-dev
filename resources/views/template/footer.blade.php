cbe9fffec87f58a04f562816533e1f279077636c
rama11 <prof.agastyo@gmail.com>
2021-05-19 04:03:18 +0000

    footer blade

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+	<div class="pull-right hidden-xs">
+	  <b>Version</b> {{env('APP_VERSION')}}
+	</div>
+	<strong>Copyright &copy; {{date('Y')}} <a href="http://www.sinergy.co.id">Sinergy Informasi Pratama</a>.</strong>
+</footer>
