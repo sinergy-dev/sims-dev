33b8a3573bfa9d64ce87551ebdd0a98556f65894
ladinar <ladinarnanda@gmail.com>
2021-06-16 13:51:09 +0700

    update sidebar blade

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -29,7 +29,15 @@
 					<ul class="treeview-menu">
 						@foreach($group['children'] as $chid)
 							<li class="activeable_menu">
-								<a href="{{url($chid->url)}}"><i class="fa fa-circle-o"></i>{{$chid->name}}</a>
+								<a href="{{url($chid->url)}}">
+									<i class="fa fa-circle-o"></i>{{$chid->name}}
+									
+									@if($chid->notif_status == "on")
+										<span class="pull-right-container">
+							    		<small class="label pull-right bg-red" id="{!! str_replace(' ', '_', $chid->name) !!}"></small>
+							    	</span>
+									@endif
+								</a>
 							</li>
 						@endforeach
 					</ul>
@@ -38,3 +46,72 @@
 		</ul>
 	</section>
 </aside>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.6.3/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.6.3/firebase-database.js"></script>
+<script type="text/javascript">
+	var firebaseConfig = {
+	    apiKey: "AIzaSyB0MWK6KLjhJlY7cL7G6STOCVGnxzjapXU",
+	    authDomain: "sims-22e41.firebaseapp.com",
+	    projectId: "sims-22e41",
+	    storageBucket: "sims-22e41.appspot.com",
+	    messagingSenderId: "84983392260",
+	    appId: "1:84983392260:web:e10924f37a7a5c189cad51",
+	    measurementId: "G-WK8FWEW0TV"
+	};
+  	// Initialize Firebase
+  	firebase.initializeApp(firebaseConfig);
+
+  	// var db = firebase.database();
+  	// var notifRef = db.ref('notif');
+
+  	// notifRef.on('value', showData, callbackError);
+
+   var firebaseRootRef = firebase.database().ref();
+
+   	if ("{{Auth::User()->id_division}}" == 'SALES' || "{{Auth::User()->id_division}}" == 'TECHNICAL PRESALES') {
+	 		var personale_Ref = firebaseRootRef.child('notif/Lead_Register');
+   	}else if ("{{Auth::User()->id_division}}" == 'FINANCE') {
+	 		var personale_Ref = firebaseRootRef.child('notif/ID_Project');
+   	}
+
+    if ("{{Auth::User()->id_division}}" == 'SALES' || "{{Auth::User()->id_division}}" == 'TECHNICAL PRESALES' || "{{Auth::User()->id_division}}" == 'FINANCE') {
+    	personale_Ref.orderByChild("to").equalTo("{{Auth::User()->email}}").on("value", function(snapshot) {
+    	console.log(snapshot.val() + 'hello')
+
+	    	if (snapshot.exists()) {
+	    		snapshot_key = snapshot.key
+		      	snapshot_child = snapshot.val() 
+			    $(".label").each(function(){
+			    	var id 	= $(this).attr("id")
+					if (id == snapshot_key) {
+			         var keys = Object.keys(snapshot.val())
+			         for (var i = 0; i < keys.length; i++) {
+			         	if (snapshot_child[keys[i]].to == "{{Auth::User()->email}}" && snapshot_child[keys[i]].total != 0) {
+							$("#"+id).text(snapshot_child[keys[i]].total)
+			         	}
+			         }
+					}
+				});
+	    	}
+	    });	
+    }
+    
+  // $("#Lead_Register").text("haha")
+
+  // function showData(items){
+  // 	$("#Lead_Register").text(items.val()["ID Project"].total)
+  // 	console.log(items.val()["ID Project"].total)
+  // 	console.log("upss")
+
+  // }
+
+  // firebase.database().ref('notif/Lead_Register/territory_3').set({
+  // 		to:"andre@sinergy.co.id",
+  //     total:77
+  // });
+
+  // function callbackError(err){
+  // 	console.log(err)
+  // }
+</script>
