5151e4f0addee88dae02a75fee43baf49a00ba8d
ladinar <ladinarnanda@gmail.com>
2021-07-16 11:09:58 +0700

    show amount in export excel pr

--- app/Http/Controllers/PrController.php
+++ app/Http/Controllers/PrController.php
@@ -14,6 +14,8 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 
 class PrController extends Controller
 {
@@ -789,7 +791,7 @@ class PrController extends Controller
         $spreadsheet->removeSheetByIndex(0);
         $sheet = $spreadsheet->getActiveSheet();
 
-        $sheet->mergeCells('A1:O1');
+        $sheet->mergeCells('A1:P1');
         $normalStyle = [
             'font' => [
                 'name' => 'Calibri',
@@ -799,25 +801,28 @@ class PrController extends Controller
 
         $titleStyle = $normalStyle;
         $titleStyle['alignment'] = ['horizontal' => Alignment::HORIZONTAL_CENTER];
+        $titleStyle['borders'] = ['outline' => ['borderStyle' => Border::BORDER_THIN]];
+        $titleStyle['fill'] = ['fillType' => Fill::FILL_SOLID, 'startColor' => ["argb" => "FFFCD703"]];
         $titleStyle['font']['bold'] = true;
 
-        $sheet->getStyle('A1:O1')->applyFromArray($titleStyle);
+        $sheet->getStyle('A1:P1')->applyFromArray($titleStyle);
         $sheet->setCellValue('A1','Purchase Request');
 
         $headerStyle = $normalStyle;
         $headerStyle['font']['bold'] = true;
-        $sheet->getStyle('A2:O2')->applyFromArray($headerStyle);;
+        $sheet->getStyle('A2:P2')->applyFromArray($headerStyle);;
 
-        $headerContent = ["No", "NO PR", "POSITION", "TYPE OF LETTER", "MONTH",  "DATE", "TO" , "ATTENTION", "TITLE", "PROJECT", "DESCRIPTION", "FROM", "DIVISION", "ISSUANCE" ,"ID PROJECT"];
+        $headerContent = ["No", "NO PR", "POSITION", "TYPE OF LETTER", "MONTH",  "DATE", "TO" , "ATTENTION", "TITLE", "PROJECT", "DESCRIPTION", "FROM", "DIVISION", "ISSUANCE" ,"ID PROJECT", "AMOUNT"];
         $sheet->fromArray($headerContent,NULL,'A2');
 
         $dataPR = PR::join('users', 'users.nik', '=', 'tb_pr.from')
-            ->select('no_pr','position','type_of_letter', 'month', 'date', 'to', 'attention', 'title','project','description','name','division','issuance','project_id')
+            ->select('no_pr','position','type_of_letter', 'month', 'date', 'to', 'attention', 'title','project','description','name','division','issuance','project_id','amount')
             ->get();
 
-        $dataPR =  $dataPR->map(function($item,$key) use ($sheet){
-            $sheet->fromArray(array_merge([$key + 1],array_values($item->toArray())),NULL,'A' . ($key + 3));
-        });
+        foreach ($dataPR as $key => $data) {
+            $data->amount = number_format($data->amount,2,",",".");
+            $sheet->fromArray(array_merge([$key + 1],array_values($data->toArray())),NULL,'A' . ($key + 3));
+        }
 
         $sheet->getColumnDimension('A')->setAutoSize(true);
         $sheet->getColumnDimension('B')->setAutoSize(true);
@@ -825,15 +830,17 @@ class PrController extends Controller
         $sheet->getColumnDimension('D')->setAutoSize(true);
         $sheet->getColumnDimension('E')->setAutoSize(true);
         $sheet->getColumnDimension('F')->setAutoSize(true);
-        $sheet->getColumnDimension('G')->setAutoSize(true);
+        $sheet->getColumnDimension('G')->setWidth(35);
         $sheet->getColumnDimension('H')->setWidth(25);
-        $sheet->getColumnDimension('I')->setAutoSize(true);
+        $sheet->getColumnDimension('I')->setWidth(50);
         $sheet->getColumnDimension('J')->setWidth(25);
         $sheet->getColumnDimension('K')->setWidth(25);
         $sheet->getColumnDimension('L')->setAutoSize(true);
         $sheet->getColumnDimension('M')->setAutoSize(true);
         $sheet->getColumnDimension('N')->setWidth(25);
-        $sheet->getColumnDimension('O')->setWidth(25);
+        $sheet->getColumnDimension('O')->setWidth(45);
+        $sheet->getColumnDimension('P')->setWidth(25);
+
 
         $fileName = 'Daftar Buku Admin (PR) ' . date('Y') . '.xlsx';
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
