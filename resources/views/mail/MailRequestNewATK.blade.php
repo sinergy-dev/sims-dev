f56e578ed2af3301923e7bb7399b0891de0b31e3
ladinar <ladinarnanda@gmail.com>
2021-07-21 10:10:13 +0700

    enhance view mail request new atk

--- resources/views/mail/MailRequestNewATK.blade.php
+++ resources/views/mail/MailRequestNewATK.blade.php
@@ -1,93 +1,129 @@
-<style type="text/css">
-	.tableBarang{
-		border-collapse: collapse;
-		width: 100%;
-	}
-	.tableBarang tr th{
-		border: 1px solid #ddd;;
-		text-align:left;
-		padding: 5px
-	}
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+	<style type="text/css">
+		table {
+		  border-collapse: collapse;
+		}
 
-	.tableBarang tr td{
-		border: 1px solid #ddd;;
-		text-align:left;
-		padding: 5px
-	}
-</style>
-<div style="color: #141414;font-family: 'Source Sans Pro','Helvetica Neue',Helvetica,Arial,sans-serif;">
-	<p>
-		Hello {{$get_user->name}},
-	</p>
-	<b><i>Request New Asset</i></b>, berikut rinciannya:
-	<table style="text-align: left;margin: 5px;">
-		<tr>
-			<th>Request By</th>
-			<th> : </th>
-			<td>{{$req_atk['nama_peminjam']}}</td>
-		</tr>
-		<tr>
-			<th>Request Date</th>
-			<th> : </th>
-			<td>{{date('d-M-Y', strtotime($req_atk['request_date']))}}</td>
-		</tr>
-	</table>
-	<table style="text-align: left;margin: 5px;" class="tableBarang">
-		<tr style="border: solid 1px">
-			<th width="40%">Name / Merk</th>
-			<th width="5%">Qty</th>
-			@if($status == 'REQUEST')
-				<th width="55%">Link</th>
-			@else
-				<th width="55%">Note</th>
-			@endif			
-		</tr>
-		@foreach($req_atk['variable'] as $data)
-		<tr>
-			@if($status == 'REQUEST')
-				<td>{{$data['nama']}}</td>
-				<td>{{$data['qty']}}</td>
-				<td><a href="{{$data['link']}}" target="_blank">{!!substr($data['link'],0,35)!!}...</a></td>
-			@else
-				<td>{{$data['nama_barang']}}</td>
-				<td>{{$data['qty_akhir']}}</td>
-				<td>{{$data['keterangan']}}</td>
-			@endif
-		</tr>
-		@endforeach	
-	</table>
-	<br>
-	Silahkan klik link berikut ini untuk melihat Detail Request ATK.<br>
-	<table width="100%" cellspacing="0" cellpadding="0">
-		<tr>
-			<td>
-				<table cellspacing="0" cellpadding="0">
+		table, th, td {
+		  border: 0px solid grey;
+		  padding-top: 14px;
+		}
+
+		table, th {
+			padding-left: 15px;
+		}
+
+		#bg_ket {
+			border-radius: 10px;
+		}
+
+		#bg_cuti {
+			border-radius: 2px;
+		}
+
+		#txt_center {
+			text-align: center;
+		}
+
+		.money:before{
+			content:"Rp";
+		}
+
+		/*.centered{
+			position: absolute;
+		  	top: 50%;
+		  	left: 50%;
+		  	transform: translate(-50%, -40%);
+		}*/
+
+		.button {
+			border: none;
+			color: white;
+			padding: 15px 32px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+			margin: 4px 2px;
+			cursor: pointer;
+			background-color: #7868e6;
+			border-radius: 4px;
+		}
+	</style>
+</head>
+<body style="display:block;width:600px;margin-left:auto;margin-right:auto;color: #000000">
+	<div style="line-height: 1.5em">
+		<center><img src="{{ asset('image/atk_req.png')}}" style="width: 50%; height: 50%;"></center>
+	</div>
+	<div style="line-height: 1.5em;padding-left: 13px;">
+		<div style="font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+			<p>
+				Hello {{$get_user->name}},
+			</p>
+			<p>
+				Request New Asset, berikut rinciannya:
+			</p>
+			<div id="bg_ket" style="background-color: #ececec; padding: 10px">
+				<table style="text-align: left;margin: 5px; font-size: 14px">
 					<tr>
-						<td style="border-radius: 2px;" bgcolor="#ED2939">
-							<a href="{{url('/asset_atk')}}#peminjaman_asset_atk" target="_blank" style="padding: 8px 12px; border: 1px solid #ED2939;border-radius: 2px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block;">
-								Request ATK
-							</a>
-						</td>
+						<th>Request By</th>
+						<th> : </th>
+						<td>{{$req_atk['nama_peminjam']}}</td>
+					</tr>
+					<tr>
+						<th>Request Date</th>
+						<th> : </th>
+						<td>{{date('d-M-Y', strtotime($req_atk['request_date']))}}</td>
 					</tr>
 				</table>
-			</td>
-		</tr>
-	</table>
-	<p>
-		Mohon di periksa kembali, jika ada kesalahan atau pertanyaan silahkan hubungi Team Developer (Ext: 384) atau email ke development@sinergy.co.id.
-	</p>
-	<p>
-		Thanks<br>
-		Best Regard,
-	</p>
-	<h5 style="color: #f39c12 !important;margin-top: 0px" class="text-yellow" ><i>Tech - Dev</i></h5>
-	<p>
-		----------------------------------------<br>
-		PT. Sinergy Informasi Pratama (SIP)<br>
-		| Inlingua Building 2nd Floor |<br>
-		| Jl. Puri Raya, Blok A 2/3 No. 33-35 | Puri Indah |<br>
-		| Kembangan | Jakarta 11610 – Indonesia |<br>
-		| Phone | 021 - 58355599 |<br>
-		----------------------------------------<br>
-	</p>
-</div>
+				<table style="text-align: left;margin: 5px; font-size: 14px">
+					<tr>
+						<th width="40%">Name / Merk</th>
+						<th width="5%">Qty</th>
+						@if($status == 'REQUEST')
+							<th width="55%">Link</th>
+						@else
+							<th width="55%">Note</th>
+						@endif			
+					</tr>
+					@foreach($req_atk['variable'] as $data)
+					<tr style="text-align: left; margin: 10px; font-size: 14px">
+						@if($status == 'REQUEST')
+							<td>{{$data['nama']}}</td>
+							<td>{{$data['qty']}}</td>
+							<td><a href="{{$data['link']}}" target="_blank">{!!substr($data['link'],0,35)!!}...</a></td>
+						@else
+							<td>{{$data['nama_barang']}}</td>
+							<td>{{$data['qty_akhir']}}</td>
+							<td>{{$data['keterangan']}}</td>
+						@endif
+					</tr>
+					@endforeach
+				</table>
+			</div>
+			<p style="font-size: 16px">
+				Silahkan klik link berikut ini untuk melihat Detail Request ATK.<br>
+			</p>
+			<center><a href="{{url('/asset_atk')}}#peminjaman_asset_atk" target="_blank"><button class="button">Request ATK</button></a></center>
+			<br>
+		</div>
+	</div>
+</body>
+<footer style="display:block;width:600px;margin-left:auto;margin-right:auto;">
+	<div style="background-color: #7868e6; padding: 20px; color: #ffffff; font-size: 12px; font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+		<p>
+			<center>PT. Sinergy Informasi Pratama</center>
+		</p>
+		<p>
+			<center>Jl. Puri Raya, Blok A 2/3 No. 33-35 Puri Indah, Kembangan, Jakarta, Indonesia 11610</center>
+		</p>
+		<p>
+			<center><i class="fa fa-phone"></i>021 - 58355599</center>
+		</p>
+	</div>
+</footer>
+</html>
