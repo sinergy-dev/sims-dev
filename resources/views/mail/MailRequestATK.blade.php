9e273548cbcfaf83635e5a398ecf61707fbeefe8
ladinar <ladinarnanda@gmail.com>
2021-07-21 10:09:43 +0700

    enhance view mail request atk

--- resources/views/mail/MailRequestATK.blade.php
+++ resources/views/mail/MailRequestATK.blade.php
@@ -1,195 +1,355 @@
-<style type="text/css">
-</style>
-<div style="color: #141414;font-family: 'Source Sans Pro','Helvetica Neue',Helvetica,Arial,sans-serif;">
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+	<style type="text/css">
+		table {
+		  border-collapse: collapse;
+		}
+
+		table, th, td {
+		  border: 0px solid grey;
+		  padding-top: 14px;
+		}
+
+		table, th {
+			padding-left: 15px;
+		}
+
+		#bg_ket {
+			border-radius: 10px;
+		}
+
+		#bg_cuti {
+			border-radius: 2px;
+		}
+
+		#txt_center {
+			text-align: center;
+		}
+
+		.money:before{
+			content:"Rp";
+		}
+
+		/*.centered{
+			position: absolute;
+		  	top: 50%;
+		  	left: 50%;
+		  	transform: translate(-50%, -40%);
+		}*/
+
+		.button {
+			border: none;
+			color: white;
+			padding: 15px 32px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+			margin: 4px 2px;
+			cursor: pointer;
+			background-color: #7868e6;
+			border-radius: 4px;
+		}
+	</style>
+</head>
+<body style="display:block;width:600px;margin-left:auto;margin-right:auto;color: #000000">
 	@if($req_atk->status == 'ACCEPT')
-		<p>
-			Hello {{$req_atk->name}},
-			<br><b>Request ATK diapprove,</b> berikut rinciannya:
-		</p>
-		<table style="text-align: left;margin: 5px;">
-			<tr>
-				<th>Nama Barang</th>
-				<th> : </th>
-				<td>{{$req_atk->nama_barang}}</td>
-			</tr>
-			<tr>
-				<th>Quantity</th>
-				<th> : </th>
-				<td>{{$req_atk->qty_akhir}}</td>
-			</tr>
-			<tr>
-				<th>Tanggal Request ATK</th>
-				<th> : </th>
-				<td>{{date('d-M-Y', strtotime($req_atk->created_at))}}</td>
-			</tr>
-			<tr>
-				<th>Note</th>
-				<th> : </th>
-				<td>{{$req_atk->keterangan}}</td>
-			</tr>
-		</table>
-		<br>
+		<div style="line-height: 1.5em">
+			<center><img src="{{ asset('image/atk_accept.png')}}" style="width: 50%; height: 50%;"></center>
+		</div>
+		<div style="line-height: 1.5em;padding-left: 13px;">
+			<div style="font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+				<p>
+					Hello {{$req_atk->name}},
+				</p>
+				<p>
+					<br><b>Request ATK diapprove,</b> berikut rinciannya:
+				</p>
+				<div id="bg_ket" style="background-color: #ececec; padding: 10px">
+					<table style="text-align: left;margin: 5px; font-size: 14px">
+						<tr>
+							<th>Nama Barang</th>
+							<th> : </th>
+							<td>{{$req_atk->nama_barang}}</td>
+						</tr>
+						<tr>
+							<th>Quantity</th>
+							<th> : </th>
+							<td>{{$req_atk->qty_akhir}}</td>
+						</tr>
+						<tr>
+							<th>Tanggal Request ATK</th>
+							<th> : </th>
+							<td>{{date('d-M-Y', strtotime($req_atk->created_at))}}</td>
+						</tr>
+						<tr>
+							<th>Note</th>
+							<th> : </th>
+							<td>{{$req_atk->keterangan}}</td>
+						</tr>
+					</table>
+				</div>
+				<p style="font-size: 16px">
+					Silahkan klik link berikut ini untuk melihat Detail Request ATK.<br>
+				</p>
+				<center><a href="{{url('/asset_atk')}}#peminjaman_asset_atk" target="_blank"><button class="button">Request ATK</button></a></center>
+				<p style="font-size: 16px">
+					Mohon di periksa kembali, jika ada kesalahan atau pertanyaan silahkan hubungi Team Developer (Ext: 384) atau email ke development@sinergy.co.id.<br>
+					Terima kasih.
+				</p>
+				<p style="font-size: 16px">
+					Best Regard,
+				</p><br>
+				<p style="font-size: 16px">
+					Tech - Dev
+				</p>
+			</div>
+		</div>
+
 	@elseif($req_atk->status == 'REJECT' || $req_atk->status == 'REJECTED')
-		<p>
-			Hello {{$req_atk->name}},
-			<br><b>Maaf request ATK ditolak</b>, berikut rinciannya:
-		</p>
-		<table style="text-align: left;margin: 5px;">
-			<tr>
-				<th>Nama Barang</th>
-				<th> : </th>
-				@if($req_atk->status == 'REJECT')
-				<td>{{$req_atk->nama_barang}}</td>
-				@else
-				<td>{{$req_atk->nama}}</td>
-				@endif
-			</tr>
-			<tr>
-				<th>Quantity</th>
-				<th> : </th>
-				@if($req_atk->status == 'REJECT')
-				<td>{{$req_atk->qty_akhir}}</td>
-				@else
-				<td>{{$req_atk->qty}}</td>
-				@endif
-			</tr>
-			<tr>
-				<th>Tanggal Request ATK</th>
-				<th> : </th>
-				<td>{{date('d-M-Y', strtotime($req_atk->created_at))}}</td>
-			</tr>
-			<tr>
-				<th>Note</th>
-				<th> : </th>
-				@if($req_atk->status == 'REJECT')
-				<td>{{$req_atk->note}}</td>
-				@else
-				<td>{{$req_atk->note_reject}}</td>
-				@endif
-			</tr>
-		</table>
-		<br>
+		<div style="line-height: 1.5em">
+			<center><img src="{{ asset('image/atk_reject.png')}}" style="width: 50%; height: 50%;"></center>
+		</div>
+		<div style="line-height: 1.5em;padding-left: 13px;">
+			<div style="font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+				<p>
+					Hello {{$req_atk->name}},
+				</p>
+				<p>
+					<br><b>Maaf request ATK ditolak</b>, berikut rinciannya:
+				</p>
+				<div id="bg_ket" style="background-color: #ececec; padding: 10px">
+					<table style="text-align: left;margin: 5px; font-size: 14px">
+						<tr>
+							<th>Nama Barang</th>
+							<th> : </th>
+							@if($req_atk->status == 'REJECT')
+							<td>{{$req_atk->nama_barang}}</td>
+							@else
+							<td>{{$req_atk->nama}}</td>
+							@endif
+						</tr>
+						<tr>
+							<th>Quantity</th>
+							<th> : </th>
+							@if($req_atk->status == 'REJECT')
+							<td>{{$req_atk->qty_akhir}}</td>
+							@else
+							<td>{{$req_atk->qty}}</td>
+							@endif
+						</tr>
+						<tr>
+							<th>Tanggal Request ATK</th>
+							<th> : </th>
+							<td>{{date('d-M-Y', strtotime($req_atk->created_at))}}</td>
+						</tr>
+						<tr>
+							<th>Note</th>
+							<th> : </th>
+							@if($req_atk->status == 'REJECT')
+							<td>{{$req_atk->note}}</td>
+							@else
+							<td>{{$req_atk->note_reject}}</td>
+							@endif
+						</tr>
+					</table>
+				</div>
+				<p style="font-size: 16px">
+					Silahkan klik link berikut ini untuk melihat Detail Request ATK.<br>
+				</p>
+				<center><a href="{{url('/asset_atk')}}#peminjaman_asset_atk" target="_blank"><button class="button">Request ATK</button></a></center>
+				<p style="font-size: 16px">
+					Mohon di periksa kembali, jika ada kesalahan atau pertanyaan silahkan hubungi Team Developer (Ext: 384) atau email ke development@sinergy.co.id.<br>
+					Terima kasih.
+				</p>
+				<p style="font-size: 16px">
+					Best Regard,
+				</p><br>
+				<p style="font-size: 16px">
+					Tech - Dev
+				</p>
+			</div>
+		</div>
+
 	@elseif($req_atk->status == 'PROCESS')
-		<p>
-			Hello {{$req_atk->name}},
-			<br><b>Request ATK anda sedang diproses,</b> berikut rinciannya:
-		</p>
-		<table style="text-align: left;margin: 5px;">
-			<tr>
-				<th>Nama Barang</th>
-				<th> : </th>
-				<td>{{$req_atk->nama}}</td>
-			</tr>
-			<tr>
-				<th>Quantity</th>
-				<th> : </th>
-				<td>{{$req_atk->qty}}</td>
-			</tr>
-			<tr>
-				<th>Tanggal Request ATK</th>
-				<th> : </th>
-				<td>{{date('d-M-Y', strtotime($req_atk->created_at))}}</td>
-			</tr>
-			<tr>
-				<th>Note</th>
-				<th> : </th>
-				<td>{{$req_atk->keterangan}}</td>
-			</tr>
-		</table>
-		<br>
+		<div style="line-height: 1.5em">
+			<center><img src="{{ asset('image/atk_process.png')}}" style="width: 50%; height: 50%;"></center>
+		</div>
+		<div style="line-height: 1.5em;padding-left: 13px;">
+			<div style="font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+				<p>
+					Hello {{$req_atk->name}},
+				</p>
+				<p>
+					<br><b>Request ATK anda sedang diproses,</b> berikut rinciannya:
+				</p>
+				<div id="bg_ket" style="background-color: #ececec; padding: 10px">
+					<table style="text-align: left;margin: 5px; font-size: 14px">
+						<tr>
+							<th>Nama Barang</th>
+							<th> : </th>
+							<td>{{$req_atk->nama}}</td>
+						</tr>
+						<tr>
+							<th>Quantity</th>
+							<th> : </th>
+							<td>{{$req_atk->qty}}</td>
+						</tr>
+						<tr>
+							<th>Tanggal Request ATK</th>
+							<th> : </th>
+							<td>{{date('d-M-Y', strtotime($req_atk->created_at))}}</td>
+						</tr>
+						<tr>
+							<th>Note</th>
+							<th> : </th>
+							<td>{{$req_atk->keterangan}}</td>
+						</tr>
+					</table>
+				</div>
+				<p style="font-size: 16px">
+					Silahkan klik link berikut ini untuk melihat Detail Request ATK.<br>
+				</p>
+				<center><a href="{{url('/asset_atk')}}#peminjaman_asset_atk" target="_blank"><button class="button">Request ATK</button></a></center>
+				<p style="font-size: 16px">
+					Mohon di periksa kembali, jika ada kesalahan atau pertanyaan silahkan hubungi Team Developer (Ext: 384) atau email ke development@sinergy.co.id.<br>
+					Terima kasih.
+				</p>
+				<p style="font-size: 16px">
+					Best Regard,
+				</p><br>
+				<p style="font-size: 16px">
+					Tech - Dev
+				</p>
+			</div>
+		</div>
+
 	@elseif($req_atk->status == 'DONE')
-		<p>
-			Hello {{$req_atk->name}},
-			<br><b>Barang sudah datang,</b> berikut rinciannya:
-		</p>
-		<table style="text-align: left;margin: 5px;">
-			<tr>
-				<th>Nama Barang</th>
-				<th> : </th>
-				<td>{{$req_atk->nama}}</td>
-			</tr>
-			<tr>
-				<th>Quantity</th>
-				<th> : </th>
-				<td>{{$req_atk->qty}}</td>
-			</tr>
-			<tr>
-				<th>Tanggal Request ATK</th>
-				<th> : </th>
-				<td>{{date('d-M-Y', strtotime($req_atk->created_at))}}</td>
-			</tr>
-			<tr>
-				<th>Note</th>
-				<th> : </th>
-				<td>{{$req_atk->keterangan}}</td>
-			</tr>
-		</table>
-		<br>
+		<div style="line-height: 1.5em">
+			<center><img src="{{ asset('image/atk_done.png')}}" style="width: 50%; height: 50%;"></center>
+		</div>
+		<div style="line-height: 1.5em;padding-left: 13px;">
+			<div style="font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+				<p>
+					Hello {{$req_atk->name}},
+				</p>
+				<p>
+					<br><b>Barang sudah datang,</b> berikut rinciannya:
+				</p>
+				<div id="bg_ket" style="background-color: #ececec; padding: 10px">
+					<table style="text-align: left;margin: 5px; font-size: 14px">
+						<tr>
+							<th>Nama Barang</th>
+							<th> : </th>
+							<td>{{$req_atk->nama}}</td>
+						</tr>
+						<tr>
+							<th>Quantity</th>
+							<th> : </th>
+							<td>{{$req_atk->qty}}</td>
+						</tr>
+						<tr>
+							<th>Tanggal Request ATK</th>
+							<th> : </th>
+							<td>{{date('d-M-Y', strtotime($req_atk->created_at))}}</td>
+						</tr>
+						<tr>
+							<th>Note</th>
+							<th> : </th>
+							<td>{{$req_atk->keterangan}}</td>
+						</tr>
+					</table>
+				</div>
+				<p style="font-size: 16px">
+					Silahkan klik link berikut ini untuk melihat Detail Request ATK.<br>
+				</p>
+				<center><a href="{{url('/asset_atk')}}#peminjaman_asset_atk" target="_blank"><button class="button">Request ATK</button></a></center>
+				<p style="font-size: 16px">
+					Mohon di periksa kembali, jika ada kesalahan atau pertanyaan silahkan hubungi Team Developer (Ext: 384) atau email ke development@sinergy.co.id.<br>
+					Terima kasih.
+				</p>
+				<p style="font-size: 16px">
+					Best Regard,
+				</p><br>
+				<p style="font-size: 16px">
+					Tech - Dev
+				</p>
+			</div>
+		</div>
+
 	@else
-		<p>
-			Hello {{$get_user->name}},
-			<br><b>Berikut request ATK</b> oleh:
-		</p>
-		<table style="text-align: left;margin: 5px;">
-			<tr>
-				<th>Request By</th>
-				<th> : </th>
-				<td>{{$req_atk->name}}</td>
-			</tr>
-			<tr>
-				<th>Nama Barang</th>
-				<th> : </th>
-				<td>{{$req_atk->nama_barang}}</td>
-			</tr>
-			<tr>
-				<th>Quantity</th>
-				<th> : </th>
-				<td>{{$req_atk->qty_akhir}}</td>
-			</tr>
-			<tr>
-				<th>Tanggal Request ATK</th>
-				<th> : </th>
-				<td>{{date('d-M-Y', strtotime($req_atk->created_at))}}</td>
-			</tr>
-			<tr>
-				<th>Note</th>
-				<th> : </th>
-				<td>{{$req_atk->keterangan}}</td>
-			</tr>
-		</table>
-		<br>
+		<div style="line-height: 1.5em">
+			<center><img src="{{ asset('image/atk_done.png')}}" style="width: 50%; height: 50%;"></center>
+		</div>
+		<div style="line-height: 1.5em;padding-left: 13px;">
+			<div style="font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+				<p>
+					Hello {{$get_user->name}},
+				</p>
+				<p>
+					<br><b>Berikut request ATK</b> oleh:
+				</p>
+				<div id="bg_ket" style="background-color: #ececec; padding: 10px">
+					<table style="text-align: left;margin: 5px; font-size: 14px">
+						<tr>
+							<th>Request By</th>
+							<th> : </th>
+							<td>{{$req_atk->name}}</td>
+						</tr>
+						<tr>
+							<th>Nama Barang</th>
+							<th> : </th>
+							<td>{{$req_atk->nama_barang}}</td>
+						</tr>
+						<tr>
+							<th>Quantity</th>
+							<th> : </th>
+							<td>{{$req_atk->qty_akhir}}</td>
+						</tr>
+						<tr>
+							<th>Tanggal Request ATK</th>
+							<th> : </th>
+							<td>{{date('d-M-Y', strtotime($req_atk->created_at))}}</td>
+						</tr>
+						<tr>
+							<th>Note</th>
+							<th> : </th>
+							<td>{{$req_atk->keterangan}}</td>
+						</tr>
+					</table>
+				</div>
+				<p style="font-size: 16px">
+					Silahkan klik link berikut ini untuk melihat Detail Request ATK.<br>
+				</p>
+				<center><a href="{{url('/asset_atk')}}#peminjaman_asset_atk" target="_blank"><button class="button">Request ATK</button></a></center>
+				<p style="font-size: 16px">
+					Mohon di periksa kembali, jika ada kesalahan atau pertanyaan silahkan hubungi Team Developer (Ext: 384) atau email ke development@sinergy.co.id.<br>
+					Terima kasih.
+				</p>
+				<p style="font-size: 16px">
+					Best Regard,
+				</p><br>
+				<p style="font-size: 16px">
+					Tech - Dev
+				</p>
+			</div>
+		</div>
 	@endif
-	Silahkan klik link berikut ini untuk melihat Detail Request ATK.<br>
-	<table width="100%" cellspacing="0" cellpadding="0">
-		<tr>
-			<td>
-				<table cellspacing="0" cellpadding="0">
-					<tr>
-						<td style="border-radius: 2px;" bgcolor="#ED2939">
-							<a href="{{url('/asset_atk')}}#peminjaman_asset_atk" target="_blank" style="padding: 8px 12px; border: 1px solid #ED2939;border-radius: 2px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block;">
-								Request ATK
-							</a>
-						</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-	</table>
-	<p>
-		Mohon di periksa kembali, jika ada kesalahan atau pertanyaan silahkan hubungi Team Developer (Ext: 384) atau email ke development@sinergy.co.id.
-	</p>
-	<p>
-		Thanks<br>
-		Best Regard,
-	</p>
-	<h5 style="color: #f39c12 !important;margin-top: 0px" class="text-yellow" ><i>Tech - Dev</i></h5>
-	<p>
-		----------------------------------------<br>
-		PT. Sinergy Informasi Pratama (SIP)<br>
-		| Inlingua Building 2nd Floor |<br>
-		| Jl. Puri Raya, Blok A 2/3 No. 33-35 | Puri Indah |<br>
-		| Kembangan | Jakarta 11610 – Indonesia |<br>
-		| Phone | 021 - 58355599 |<br>
-		----------------------------------------<br>
-	</p>
-</div>
+</body>
+<footer style="display:block;width:600px;margin-left:auto;margin-right:auto;">
+	<div style="background-color: #7868e6; padding: 20px; color: #ffffff; font-size: 12px; font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+		<p>
+			<center>PT. Sinergy Informasi Pratama</center>
+		</p>
+		<p>
+			<center>Jl. Puri Raya, Blok A 2/3 No. 33-35 Puri Indah, Kembangan, Jakarta, Indonesia 11610</center>
+		</p>
+		<p>
+			<center><i class="fa fa-phone"></i>021 - 58355599</center>
+		</p>
+	</div>
+</footer>
+</html>
