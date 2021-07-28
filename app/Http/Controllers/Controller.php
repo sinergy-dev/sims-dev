af82b297462e0cb67ab1d5302e254ff40291bddf
ladinar <ladinarnanda@gmail.com>
2021-07-28 16:34:01 +0700

    update controller php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -15,7 +15,7 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function initMenuBase(){
-    	$listMenuAll = DB::table('features')
+    		$listMenuAll = DB::table('features')
 			->orderBy('index_of','ASC')
 			->whereIn('id',DB::table('roles_feature')
 				->whereIn('role_id',DB::table('role_user')
@@ -25,36 +25,40 @@ class Controller extends BaseController
 			->orderBy('index_of','ASC')
 			->get();
 
-		$groups = [];
-		foreach($listMenuAll->pluck('group')->unique()->values() as $group){
-			$listMenuEach = $listMenuAll->where('group',$group)->values();
-			array_push($groups, [
-				"text" => $group,
-				"icon_group" => $listMenuEach[0]->icon_group,
-				"children" => $listMenuEach
-			]);
-		}
-		$role_user = DB::table('role_user')->where('user_id','=',Auth::User()->nik)->first();
-		if(!$role_user){
-			$role_user = 1;
-		} else {
-			$role_user = $role_user->role_id;
-		}
+			$groups = [];
+			foreach($listMenuAll->pluck('group')->unique()->values() as $group){
+				$listMenuEach = $listMenuAll->where('group',$group)->values();
+				array_push($groups, [
+					"text" => $group,
+					"status" => $listMenuEach[0]->notif_status,
+					"name"=>$listMenuEach[0]->name,
+					"icon_group" => $listMenuEach[0]->icon_group,
+					"children" => $listMenuEach
+				]);
+			}
+			$role_user = DB::table('role_user')->where('user_id','=',Auth::User()->nik)->first();
+			if(!$role_user){
+				$role_user = 1;
+			} else {
+				$role_user = $role_user->role_id;
+			}
 
-		return collect([
-			'userRole' => DB::table('roles')->where('id','=',$role_user)->first(),
-			'listMenu' => $groups
-		]);
+			return collect([
+				'userRole' => DB::table('roles')->where('id','=',$role_user)->first(),
+				'listMenu' => $groups
+			]);
+	    		// code...
     }
 
     public function RoleDynamic($group){
-    	return DB::table('feature_item')
-            ->where('group',$group)
-	          ->whereIn('id',DB::table('roles_feature_item')
-	            ->whereIn('roles_id',DB::table('role_user')
-	              ->where('user_id',Auth::User()->nik)
-	              ->pluck('role_id'))
-	            ->pluck('feature_item_id'))
-	          ->get()->pluck('item_id');
-  }
+		return DB::table('feature_item')
+        ->where('group',$group)
+          ->whereIn('id',DB::table('roles_feature_item')
+            ->whereIn('roles_id',DB::table('role_user')
+              ->where('user_id',Auth::User()->nik)
+              ->pluck('role_id'))
+            ->pluck('feature_item_id'))
+          ->get()->pluck('item_id');
+   
+  	}
 }
