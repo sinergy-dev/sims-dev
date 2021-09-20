7db8cbfee2f5ec71fdd1de1d6035db8579c97a11
rama11 <prof.agastyo@gmail.com>
2021-09-20 11:19:01 +0700

    make new controller permission config

---
+++ app/Http/Controllers/PermissionConfigController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PermissionConfigController extends Controller
+{
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function testPermissionConfig(){ 
+        return view('testPermissionConfig')->with(['initView'=> $this->initMenuBase()]);
+    }
+}
