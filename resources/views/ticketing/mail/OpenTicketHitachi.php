17555b9909ebaea676bcc59eac703464a1c52dfc
ladinar <ladinarnanda@gmail.com>
2021-12-21 16:27:18 +0700

    add open ticket hitachi

---
+++ resources/views/ticketing/mail/OpenTicketHitachi.php
@@ -0,0 +1,77 @@
+<div>
+	<p>
+		<b>
+			<span style="font-size:11pt;font-family:Calibri,sans-serif;color:black">Kepada</span>
+		</b>
+	</p>
+	<p>
+		<b>
+			<span style="font-size:11pt;font-family:Calibri,sans-serif;color:black">Team HITACHI</span>
+		</b>
+	</p>
+	<p>
+		<b>
+			<span style="font-size:11pt;font-family:Calibri,sans-serif;color:black">
+				<br>
+			</span>
+		</b>
+	</p>
+	<p>
+		<span style="font-size:11pt;font-family:Calibri,sans-serif;color:black">Mohon untuk di openkan ticket untuk perbaikan problem pada mesin ATM/CDM berikut ini :</span>
+	</p>
+	<table>
+		<tr>
+			<td style="padding:10px;font-size:11pt;font-family:Calibri,sans-serif;color:black">Name of Bank</td>
+			<td style="font-size:11pt;font-family:Calibri,sans-serif;color:black"> : </td>
+			<td class="holderCustomer" style="font-size:11pt;font-family:Calibri,sans-serif;color:black">Bank Central Asia</td>
+		</tr>
+		<tr>
+			<td style="padding:10px;font-size:11pt;font-family:Calibri,sans-serif;color:black">Machine ID</td>
+			<td style="font-size:11pt;font-family:Calibri,sans-serif;color:black"> : </td>
+			<td class="holderIDATM" style="font-size:11pt;font-family:Calibri,sans-serif;color:black">Z05U</td>
+		</tr>
+		<tr>
+			<td style="padding:10px;font-size:11pt;font-family:Calibri,sans-serif;color:black">Location</td>
+			<td style="font-size:11pt;font-family:Calibri,sans-serif;color:black"> : </td>
+			<td class="holderLocation" style="font-size:11pt;font-family:Calibri,sans-serif;color:black">CRM Gresik Kota Baru 1</td>
+		</tr>
+		<tr>
+			<td style="padding:10px;font-size:11pt;font-family:Calibri,sans-serif;color:black">Serial Number</td>
+			<td style="font-size:11pt;font-family:Calibri,sans-serif;color:black"> : </td>
+			<td class="holderSerial" style="font-size:11pt;font-family:Calibri,sans-serif;color:black">763061</td>
+		</tr>
+		<tr>
+			<td style="padding:10px;font-size:11pt;font-family:Calibri,sans-serif;color:black">Machine Type</td>
+			<td style="font-size:11pt;font-family:Calibri,sans-serif;color:black"> : </td>
+			<td class="holderType" style="font-size:11pt;font-family:Calibri,sans-serif;color:black">HOTS 1718</td>
+		</tr>
+		<tr>
+			<td style="padding:10px;font-size:11pt;font-family:Calibri,sans-serif;color:black">Contact Person</td>
+			<td style="font-size:11pt;font-family:Calibri,sans-serif;color:black"> : </td>
+			<td class="holderPIC" style="font-size:11pt;font-family:Calibri,sans-serif;color:black">Andalan Surabaya</td>
+		</tr>
+		<tr>
+			<td style="padding:10px;font-size:11pt;font-family:Calibri,sans-serif;color:black">Phone number</td>
+			<td style="font-size:11pt;font-family:Calibri,sans-serif;color:black"> : </td>
+			<td class="holderContact" style="font-size:11pt;font-family:Calibri,sans-serif;color:black">031-5956593</td>
+		</tr>
+		<tr>
+			<td style="padding:10px;font-size:11pt;font-family:Calibri,sans-serif;color:black">Problem description</td>
+			<td style="font-size:11pt;font-family:Calibri,sans-serif;color:black"> : </td>
+			<td class="holderProblem" style="font-size:11pt;font-family:Calibri,sans-serif;color:black">Error On Bunch+Down </td>
+		</tr>
+	</table>
+	<p>
+		<span style="font-size:11pt;font-family:Calibri,sans-serif;color:black">&nbsp;</span>
+	</p>
+	<p>
+		<span style="font-size:11pt;font-family:Calibri,sans-serif;color:black" lang="EN-US">Salam,</span>
+	</p>
+	<p>
+		<span style="font-size:11pt;font-family:Calibri,sans-serif;color:black" lang="EN-US">&nbsp;</span>
+	</p>
+	<p>
+		<span style="font-size:11pt;font-family:Calibri,sans-serif;color:black" lang="EN-US">{{Auth::user()->name}} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<wbr>&nbsp;&nbsp;&nbsp;&nbsp;Helpdesk</span>
+		<span style="font-size:11pt;font-family:Calibri,sans-serif;color:black">&nbsp; - PT. Sinergy Informasi Pratama</span>
+	</p>
+</div>
