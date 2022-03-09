b1966971135377bc72f1b7447f8295fc56d9350f
rama11 <prof.agastyo@gmail.com>
2022-03-09 08:52:35 +0700

    update main blade

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -59,6 +59,7 @@
 
 		<!-- jQuery 3.1.1 -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<!-- <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script> -->
 		<!-- Bootstrap 3.3.7 -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		@section('scriptNotificationHeader')
