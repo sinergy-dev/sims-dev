07bbf809ffcd544a8325057ddf92acac585df09b
rama11 <prof.agastyo@gmail.com>
2022-03-24 16:24:16 +0700

    add syslog for each handover

--- app/Console/Commands/HandoverCuti.php
+++ app/Console/Commands/HandoverCuti.php
@@ -39,12 +39,24 @@ class HandoverCuti extends Command
     public function handle()
     {
         $totalcuti = User::select('cuti2','nik','name')->where('status_karyawan','cuti')->get(); 
+        syslog(LOG_ERR, "Handover Cuti Start");
+        syslog(LOG_ERR, "-------------------------");
 
         foreach ($totalcuti as $data) {
+            syslog(LOG_ERR, "Handover Cuti for " . $data->name);
             // print_r($data->name . $data->nik . "\n");
             $update = User::where('nik',$data->nik)->first();
+            syslog(LOG_ERR, "before cuti : " . $data->cuti);
+            syslog(LOG_ERR, "before cuti2 : " . $data->cuti2);
             $data->cuti = $data->cuti2;
+
+            syslog(LOG_ERR, "-------------------------");
+            syslog(LOG_ERR, "after cuti : " . $data->cuti);
+            syslog(LOG_ERR, "after cuti2 : " . $data->cuti2);
+            syslog(LOG_ERR, "-------------------------");
+            
             $data->save();
+
         }
         //
 
