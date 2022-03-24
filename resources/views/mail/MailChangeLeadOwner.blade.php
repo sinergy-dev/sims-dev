c461611dbb73786523a901d51b5de756c8fc9f37
rama11 <prof.agastyo@gmail.com>
2022-03-24 16:20:41 +0700

    add new template for change lead owner

--- resources/views/mail/MailChangeLeadOwner.blade.php
+++ resources/views/mail/MailChangeLeadOwner.blade.php
@@ -1,44 +1,145 @@
-<div style='font-family: Arial, Helvetica, sans-serif;'>
-<h1>Change Lead Register Ownership</h1>
-This email is intended to inform you that there has been a change in Lead Register Ownership. <br>For more details, please see below.
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+	<style type="text/css">
+		table {
+		  border-collapse: collapse;
+		}
 
-<br>
-<table style="width: 100%; text-align: left;border: 1px solid black; border-collapse: collapse;">
-	<thead>
-		<tr>
-			<th style="border: 1px solid black;">Lead ID</th>
-			<th style="border: 1px solid black;">Sales Before</th>
-			<th style="border: 1px solid black;">Sales After</th>
-			<th style="border: 1px solid black;">Change Date</th>
-			<th style="border: 1px solid black;">Changer</th>
-		</tr>
-	</thead>
-	<tbody>
-		@foreach($data as $value)
-			<tr>
-				<td style="border: 1px solid black;"><a href="https://app.sinergy.co.id/detail_project/{{$value['lead_id']}}">{{$value['lead_id']}}</a></td>
-				<td style="border: 1px solid black;">{{$value['before_sales']}}</td>
-				<td style="border: 1px solid black;">{{$value['after_sales']}}</td>
-				<td style="border: 1px solid black;">{{$value['date_change']}}</td>
-				<td style="border: 1px solid black;">{{$value['changer']}}</td>
-			</tr>
-		@endforeach
-	</tbody>
-</table>
-<br>
-That is what can be conveyed.
-<p>
-	Thanks<br>
-	Best Regard,
-</p>
-<h5 style="color: #f39c12 !important;margin-top: 0px" class="text-yellow" ><i>Tech - Dev</i></h5>
-<p>
-	----------------------------------------<br>
-	PT. Sinergy Informasi Pratama (SIP)<br>
-	| Inlingua Building 2nd Floor |<br>
-	| Jl. Puri Raya, Blok A 2/3 No. 33-35 | Puri Indah |<br>
-	| Kembangan | Jakarta 11610 – Indonesia |<br>
-	| Phone | 021 - 58355599 |<br>
-	----------------------------------------<br>
-</p>
-</div>
+		table, th, td {
+		  border: 0px solid grey;
+		  padding-top: 14px;
+		}
+
+		table, th {
+			padding-left: 15px;
+		}
+
+		#bg_ket {
+			border-radius: 10px;
+		}
+
+		#bg_cuti {
+			border-radius: 2px;
+		}
+
+		#txt_center {
+			text-align: center;
+		}
+
+		.money:before{
+			content:"Rp";
+		}
+
+		/*.centered{
+			position: absolute;
+		  	top: 50%;
+		  	left: 50%;
+		  	transform: translate(-50%, -40%);
+		}*/
+
+		.button {
+			border: none;
+			color: white;
+			padding: 15px 32px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+			margin: 4px 2px;
+			cursor: pointer;
+			background-color: #7868e6;
+			border-radius: 4px;
+		}
+	</style>
+</head>
+<body style="display:block;width:600px;margin-left:auto;margin-right:auto;color: #000000">
+		<div style="line-height: 1.5em">
+			<center><img src="{{ asset('image/change_owner_new2.png')}}" style="width: 50%; height: 50%;"></center>
+		</div>
+		<div style="line-height: 1.5em;padding-left: 13px;">
+			<div style="font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+				<p style="font-size: 16px">
+					<h3>Change Lead Register Ownership</h3>
+					This email is intended to inform you that there has been a change in Lead Register Ownership. <br>For more details, please see below.<br>
+				</p>
+				<div id="bg_ket" style="background-color: #ececec; padding: 10px; text-align: justify; text-justify: inter-word;">
+					@foreach($data as $value)
+					<p>
+						Lead ID &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: <a href="https://app.sinergy.co.id/project/detailSales/{{$value['lead_id']}}">{{$value['lead_id']}}</a>
+					</p>
+					<p>
+						Sales Before &nbsp&nbsp&nbsp&nbsp: {{$value['before_sales']}}
+					</p>
+					<p>
+						Sales After &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: {{$value['after_sales']}}
+					</p>
+					<p>
+						Change Date &nbsp&nbsp: {{$value['date_change']}}
+					</p>
+					<p>
+						Changer &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: {{$value['changer']}}
+					</p>
+					<hr>
+					@endforeach
+				</div>
+				<!-- <div id="bg_ket" style="background-color: #ececec; padding: 10px">
+					<table style="text-align: left;margin: 5px; font-size: 14px" class="tableLead">
+						@foreach($data as $value)
+							<tr>
+								<th>Lead ID</th>
+								<th> : </th>
+								<th><a href="https://app.sinergy.co.id/project/detailSales/{{$value['lead_id']}}">{{$value['lead_id']}}</a></th>
+							</tr>
+							<tr>
+								<th>Sales Before</th>
+								<th> : </th>
+								<th>{{$value['before_sales']}}</th>
+							</tr>
+							<tr>
+								<th>Sales After</th>
+								<th> : </th>
+								<th>{{$value['after_sales']}}</th>
+							</tr>
+							<tr>
+								<th>Change Date</th>
+								<th> : </th>
+								<th>{{$value['date_change']}}</th>
+							</tr>
+							<tr>
+								<th>Changer</th>
+								<th> : </th>
+								<th>{{$value['changer']}}</th>
+							</tr><hr>
+						@endforeach
+					</table>
+				</div> -->
+				<p style="font-size: 16px">
+					That is what can be conveyed.<br>	
+				</p>
+				<p style="font-size: 16px">
+					Best Regard,
+				</p><br>
+				<p style="font-size: 16px">
+					Tech - Dev
+				</p>
+			</div>
+		</div>
+</body>
+<footer style="display:block;width:600px;margin-left:auto;margin-right:auto;">
+	<div style="background-color: #7868e6; padding: 20px; color: #ffffff; font-size: 12px; font-family: 'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif;">
+		<p>
+			<center>PT. Sinergy Informasi Pratama</center>
+		</p>
+		<p>
+			<center>Jl. Puri Raya, Blok A 2/3 No. 33-35 Puri Indah, Kembangan, Jakarta, Indonesia 11610</center>
+		</p>
+		<p>
+			<center><i class="fa fa-phone"></i>021 - 58355599</center>
+		</p>
+	</div>
+</footer>
+</html>
