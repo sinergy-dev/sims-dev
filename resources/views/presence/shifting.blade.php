04dc22e351deac28fb6e6ab2648f9fbf478ca0c3
rama11 <prof.agastyo@gmail.com>
2022-03-25 15:29:28 +0700

    add onclick on button editShifting

--- resources/views/presence/shifting.blade.php
+++ resources/views/presence/shifting.blade.php
@@ -369,7 +369,7 @@ Presence Shifting
 								<div class="box-header with-border">
 									<h3 class="box-title">{{$shiftingOptionKey}}</h3>
 									<div class="box-tools pull-right">
-										<button type="button" class="btn btn-box-tool" id="tambah">
+										<button type="button" class="btn btn-box-tool" onclick="editShifting({{$shiftingOptionValues[0]['id_project']}})">
 											<i class="fa fa-gear"></i>
 										</button>
 										<button type="button" class="btn btn-box-tool" data-widget="collapse">
