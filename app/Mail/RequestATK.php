db1d187f7f796a8b18e024270cdf5e278eee09a7
rama11 <prof.agastyo@gmail.com>
2022-06-24 15:13:54 +0700

    update requestATK mail

--- app/Mail/RequestATK.php
+++ app/Mail/RequestATK.php
@@ -10,18 +10,19 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 class RequestATK extends Mailable
 {
     use Queueable, SerializesModels;
-    public $customSubject, $req_atk, $sebuah_variable, $id_position, $get_user,$status;
+    public $customSubject, $req_atk, $sebuah_variable, $id_position, $id_division, $get_user,$status;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($customSubject,$req_atk,$id_position,$get_user,$status)
+    public function __construct($customSubject,$req_atk,$id_position,$id_division,$get_user,$status)
     {
         $this->customSubject = $customSubject;
         $this->req_atk = $req_atk;
         $this->id_position = $id_position;
+        $this->id_division = $id_division;
         $this->get_user = $get_user;
         $this->status = $status;
     }
@@ -33,7 +34,7 @@ class RequestATK extends Mailable
      */
     public function build()
     {
-        if($this->id_position == "HR MANAGER" || $this->id_position == 'WAREHOUSE'){
+        if($this->id_position == "HR MANAGER" || $this->id_division == 'PROCUREMENT'){
             $this->sebuah_variable = "Berikut request ATK yang harus dibuatkan PR:";
         } else {
             $this->sebuah_variable = "Request ATK mu sedang dibuatkan PR, tunggu informasi lebih lanjut, berikut rinciannya:";
