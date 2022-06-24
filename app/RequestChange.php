e207fee2940835202b7fc5f63629ee692d609069
rama11 <prof.agastyo@gmail.com>
2022-06-24 15:26:14 +0700

    add requestchange model

---
+++ app/RequestChange.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RequestChange extends Model
+{
+    //
+
+    protected $table = 'request_change';
+
+    public $timestamps = false;
+
+    protected $primaryKey = 'id';
+
+}
