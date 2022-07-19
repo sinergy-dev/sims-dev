49e854dba6b93dfb619f858388491a733c04797e
rama11 <prof.agastyo@gmail.com>
2022-07-19 09:20:40 +0700

    update partnership certificate model

--- app/PartnershipCertification.php
+++ app/PartnershipCertification.php
@@ -8,7 +8,7 @@ class PartnershipCertification extends Model
 {
     protected $table = 'tb_partnership_certification';
     protected $primaryKey = 'id';
-    protected $fillable = ['id_partnership', 'name', 'type_certification', 'name_certification'];
+    protected $fillable = ['id_partnership', 'name', 'type_certification', 'name_certification', 'expired_date', 'certificate'];
 
     public function user(){
         return $this->hasOne('App\User','name','name');
