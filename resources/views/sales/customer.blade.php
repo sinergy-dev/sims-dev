efd01129d711d5967f81df0554d7c87d9bfd7b3f
rama11 <prof.agastyo@gmail.com>
2022-12-02 17:20:18 +0700

    update customer blade

--- resources/views/sales/customer.blade.php
+++ resources/views/sales/customer.blade.php
@@ -680,11 +680,10 @@ Customer
       cRequest =  JSON.parse('@json($count_request)')
 
       if (accesable.includes('popUp')) {
-        $.each(@json($roles),function(value,item){
-          $('.AccName').text(item.name)
-          $('.AccEmail').text(item.email)
-          $('.AccPhone').text('+62' + item.phone)
-        })
+          // console.log(value)
+          $('.AccName').text(@json($roles).name)
+          $('.AccEmail').text(@json($roles).email)
+          $('.AccPhone').text('+62' + @json($roles).phone)
       }
 
       if (cRequest > 0) {
