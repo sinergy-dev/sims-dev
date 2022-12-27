eba453d03c0830b0f9ad903ecbb32ef08a982fe2
rama11 <prof.agastyo@gmail.com>
2022-12-27 10:14:12 +0700

    update pr id project

--- app/SalesProject.php
+++ app/SalesProject.php
@@ -12,15 +12,15 @@ class SalesProject extends Model
     protected $primaryKey = 'id_pro';
     protected $fillable = ['customer_name','id_project','nik', 'no_po_customer', 'id_contact','name_project', 'amount_usd','amount_idr','date','note','sales_name'];
 
-    protected $appends = ['get_pr'];
+    // protected $appends = ['get_pr'];
 
-    public function getGetPrAttribute()
-    {
-        $data = SalesProject::join('tb_pr', 'tb_pr.project_id', 'tb_id_project.id_project')
-        	->select('no_pr', 'tb_pr.amount', DB::raw("(CASE WHEN (tb_pr.title is null) THEN tb_pr.description ELSE tb_pr.title END) as title"))
-            ->where('id_project', $this->id_project)
-            ->get();
+    // public function getGetPrAttribute()
+    // {
+    //     $data = SalesProject::join('tb_pr', 'tb_pr.project_id', 'tb_id_project.id_project')
+    //     	->select('no_pr', 'tb_pr.amount', DB::raw("(CASE WHEN (tb_pr.title is null) THEN tb_pr.description ELSE tb_pr.title END) as title"))
+    //         ->where('id_project', $this->id_project)
+    //         ->get();
 
-        return $data;
-    }
+    //     return $data;
+    // }
 }
