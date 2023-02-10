49c64d24bc3a2bfda3bd31b560de435e9a723893
rama11 <prof.agastyo@gmail.com>
2023-02-10 13:26:43 +0700

    update PMO models

--- app/GanttTaskPmo.php
+++ app/GanttTaskPmo.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App;
+use DB;
+use Carbon\Carbon;
+
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -8,5 +11,22 @@ class GanttTaskPmo extends Model
 {
     protected $table = "gantt_tasks_pmo";
     public $primaryKey = "id";
-    public $timestamps = false;
+
+    protected $appends = ['open'];
+ 
+    public function getOpenAttribute(){
+        return true;
+    }
+
+    // public function getSubTaskOnGoingAttribute()
+    // {
+    //     $data = DB::table('gantt_tasks_pmo')->select('parent')->where('parent','!=',0);
+
+    //     $subTaskMilestone = DB::table('gantt_tasks_pmo')->leftJoinSub($data, 'parent_text',function($join){
+    //                 $join->on("gantt_tasks_pmo.id", '=', 'parent_text.parent');
+    //             })->select('text')->where('id',$this->id);
+
+    //     return $subTaskMilestone;
+    // }
+
 }
