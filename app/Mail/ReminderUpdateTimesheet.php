f8800bab611c9117bb60e2847a4cd412bcb267eb
rama11 <prof.agastyo@gmail.com>
2023-08-08 11:29:37 +0700

    update mail reminderupdate timesheet

--- app/Mail/ReminderUpdateTimesheet.php
+++ app/Mail/ReminderUpdateTimesheet.php
@@ -10,15 +10,17 @@ use Illuminate\Queue\SerializesModels;
 class ReminderUpdateTimesheet extends Mailable
 {
     use Queueable, SerializesModels;
+    public $all;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($all)
     {
         //
+        $this->all = $all;
     }
 
     /**
