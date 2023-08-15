6c1360c2fce57870ee5371906a062b6239d05bf3
rama11 <prof.agastyo@gmail.com>
2023-08-15 08:55:29 +0700

    update timesheet config model

--- app/TimesheetConfig.php
+++ app/TimesheetConfig.php
@@ -8,6 +8,6 @@ class TimesheetConfig extends Model
 {
     protected $table = 'tb_timesheet_config';
     protected $primaryKey = 'id';
-    protected $fillable = ['roles','phase', 'task','date_add','division'];
+    protected $fillable = ['roles','phase', 'task','date_add','division','status_assign_pid'];
     public $timestamps = false;
 }
