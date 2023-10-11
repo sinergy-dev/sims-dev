8bea859896ca7307a02cb6367e4e139a25539b96
rama11 <prof.agastyo@gmail.com>
2023-10-11 16:17:19 +0700

    update sbe blade

--- resources/views/solution/sbe.blade.php
+++ resources/views/solution/sbe.blade.php
@@ -3,12 +3,12 @@
 SBE
 @endsection
 @section('head_css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap.css">
 	<style type="text/css">
 		.dataTables_filter {
 		    display: none;
 		}
 	</style>
-  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap.css">
 @endsection
 @section('content')
 	<section class="content-header">
