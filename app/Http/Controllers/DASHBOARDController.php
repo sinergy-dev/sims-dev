759734995fbac585afeceef4ca633390cfc56abf
app.sinergy <app@sinergy.co.id>
2024-01-25 16:34:11 +0700

    update dashboard controller

--- app/Http/Controllers/DASHBOARDController.php
+++ app/Http/Controllers/DASHBOARDController.php
@@ -1377,7 +1377,8 @@ class DASHBOARDController extends Controller
         $hasil = [0,0,0,0,0,0,0,0,0,0,0,0];
         $bulan_angka = [1,2,3,4,5,6,7,8,9,10,11,12];
 
-        if (empty($chart) != 0) {
+        if ($chart) {
+            // return $chart;
             $first = $chart[0]->month;
 
             $bulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
