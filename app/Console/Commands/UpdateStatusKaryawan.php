2a802b138db298f9c71e29019b18270236c6da8d
app.sinergy <app@sinergy.co.id>
2024-03-08 17:02:45 +0700

    update backend updateStatusKaryawan

--- app/Console/Commands/UpdateStatusKaryawan.php
+++ app/Console/Commands/UpdateStatusKaryawan.php
@@ -6,6 +6,10 @@ use Illuminate\Console\Command;
 use App\User;
 use GuzzleHttp\Client;
 use DB;
+use Carbon\Carbon;
+use DatePeriod;
+use DateInterval;
+use DateTime;
 
 class UpdateStatusKaryawan extends Command
 {
@@ -40,28 +44,70 @@ class UpdateStatusKaryawan extends Command
      */
     public function handle()
     {
-        // $client = new Client();
-        // $client = $client->get('https://www.googleapis.com/calendar/v3/calendars/en.indonesian%23holiday%40group.v.calendar.google.com/events?key=' . env('GOOGLE_API_YEY'));
-        // $variable = json_decode($client->getBody())->items;
-        // $i = 0;
-        // foreach ($variable as $key => $value) {
-        //   if(strpos($value->summary,'Cuti Bersama') === 0){
-        //     if(strpos($value->start->date , date('Y')) === 0){
-        //       $i++;
-        //     }
-        //   }
-        // }
-        // $total_cuti = 12 - $i;
+        $client = new Client();
+        $startDate = Carbon::now()->format("Y-m-d");
+        $endDate = Carbon::now()->endOfYear()->format("Y-m-d");
+        $api_response = $client->get('https://www.googleapis.com/calendar/v3/calendars/en.indonesian%23holiday%40group.v.calendar.google.com/events?key='.env('GCALENDAR_API_KEY'));
+        $json = (string)$api_response->getBody();
+        $holiday_indonesia = json_decode($json, true);
+
+        $holiday_indonesia_final_detail = collect();
+        $holiday_indonesia_final_details = collect();
+        $holiday_indonesia_final_date = collect();
+        $holiday_indonesia_final_dates = collect();
+
+
+        foreach ($holiday_indonesia["items"] as $value) {
+            if(( (( $value["start"]["date"] >= $startDate ) && ( $value["start"]["date"] <= $endDate )) && (($value['summary'] == 'Idul Fitri Joint Holiday') || ($value['summary'] == 'Boxing Day')) )){
+                $holiday_indonesia_final_detail->push(["start_date" => $value["start"]["date"],"activity" => $value["summary"]]);
+                $holiday_indonesia_final_date->push($value["start"]["date"]);
+            }
+        }
+
+        $holiday_indonesia_final_detail = $holiday_indonesia_final_detail->merge($holiday_indonesia_final_details);
+
+        $period = new DatePeriod(
+             new DateTime($startDate),
+             new DateInterval('P1D'),
+             new DateTime($endDate . '23:59:59')
+        );
+
+        $workDays = collect();
+        foreach($period as $date){
+            if(!($date->format("N") == 6 || $date->format("N") == 7)){
+                $workDays->push($date->format("Y-m-d"));
+            }
+        }
+
+        $workDaysMinHoliday = $workDays->diff($holiday_indonesia_final_date->unique());
+        $workDaysMinHolidayKeyed = $workDaysMinHoliday->map(function ($item, $key) {
+            return ["date" => $item];
+            // return (object) array('date' => $item);
+            return $item;
+        });
+
+        $count = count($holiday_indonesia_final_detail);
+
+        $total_cuti = 12 - $count;
         // $reset = User::select('nik','name','status_karyawan')->where('nik',1191094060)->first();
-        $reset = DB::table('users')->select('nik','name',DB::raw('DATEDIFF(NOW(),date_of_entry) AS date_of_entrys'),'status_karyawan')->get();
+        $reset = DB::table('users')->select('nik','name',DB::raw('DATEDIFF(NOW(),date_of_entry) AS date_of_entrys'),'status_karyawan')->where('status_karyawan','!=','dummy')->get();
 
         foreach ($reset as $data) {
 
             // print_r($data->name . $data->nik . "\n");
             if ($data->date_of_entrys >= '365') {
                 if ($data->status_karyawan == 'belum_cuti') {
+                    // print_r($data->name . $data->nik . "\n");
+
                     $update = User::where('nik',$data->nik)->first();
                     $update->status_karyawan = 'cuti';
+                    // if (date("n") == 4 && date("j") == 1) {
+                        $update->cuti = 0;
+                        $update->cuti2 = $count; 
+                    // } else {
+                        // $update->cuti = $count;
+                        // $update->cuti2 = 0; 
+                    // }
                     $update->update();
                     syslog(LOG_ERR, $update->nik . " - " . $update->name . "leaving permit permission updated ");
                 }
