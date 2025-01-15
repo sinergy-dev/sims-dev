299967ea7882531dc68044f8dc9f72d5d73b63ab
app sinergy <app@sinergy.co.id>
2025-01-15 17:18:11 +0700

    add asset computer enhance

--- resources/views/asset_management/asset.blade.php
+++ resources/views/asset_management/asset.blade.php
@@ -2910,13 +2910,13 @@
               const match = pattern.exec(spesifikasi);
 
               if (match) {
-                const osVersion = match[1].trim();
-                dataForm.append("operatingSystem", osVersion);
+                const osValue = match[1].trim();
               } 
             } else {
-              osNull = "";
-              dataForm.append('operatingSystem', osNull);
+              osValue = "";
             }
+
+            dataForm.append('operatingSystem', osValue);
 
             // if its vehicle remove Nomor Polisi in spesifikasi
             if ($("#selectCategory").val() === "VHC") {
